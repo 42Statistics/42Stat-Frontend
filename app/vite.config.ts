41f521c81472a19f7b571d26fc44c7e5e443818a
Yongjun Park <yopark@student.42seoul.kr>
2023-03-08 01:56:29 +0900

    feat(dev): :sparkles: @ path

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,6 +1,7 @@
 import basicSsl from '@vitejs/plugin-basic-ssl';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,5 +10,8 @@ export default defineConfig({
   server: {
     port: 8080,
     host: 'frontend',
+  },
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
 });

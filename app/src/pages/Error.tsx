460eb74d93f97c38a7c771955206f79faf3f98fe
Yongjun Park <yopark@student.42seoul.kr>
2023-03-08 23:29:49 +0900

    feat: :sparkles: ErrorPage

---
+++ app/src/pages/Error.tsx
@@ -0,0 +1,18 @@
+import { Helmet } from 'react-helmet-async';
+import { isRouteErrorResponse, useRouteError } from 'react-router-dom';
+
+export const ErrorPage = () => {
+  const error = useRouteError();
+
+  if (isRouteErrorResponse(error)) {
+    return (
+      <>
+        <Helmet>
+          <title>{error.statusText} | 42Stat</title>
+        </Helmet>
+        {error.status} {error.statusText}
+      </>
+    );
+  }
+  return <></>;
+};

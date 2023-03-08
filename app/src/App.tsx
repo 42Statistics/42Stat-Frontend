f88aa89db6daa64d3ff6543891affedf61bdd063
Yongjun Park <yopark@student.42seoul.kr>
2023-03-08 23:27:56 +0900

    feat: :sparkles: AuthGuard 임시 제작

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -4,9 +4,11 @@ import {
   Route,
   Routes,
 } from 'react-router-dom';
+import { AuthGuard } from './guards/AuthGuard';
 import { DashboardLayout } from './layouts/DashboardLayout';
 import { NotFoundPage } from './pages/404';
 import { AboutPage } from './pages/About';
+import { ErrorPage } from './pages/Error';
 import { HomePage } from './pages/Home';
 import { LoginPage } from './pages/Login';
 import { ProfilePage } from './pages/Profile';
@@ -20,18 +22,22 @@ const AppRoutes = () => {
   return (
     <Router>
       <Routes>
-        <Route
-          path="/"
-          element={isAuthenticated ? <Navigate to="/home" /> : <LoginPage />}
-        />
-        <Route element={<DashboardLayout />}>
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/total" element={<TotalPage />} />
-          <Route path="/profile/:username" element={<ProfilePage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/settings" element={<SettingsPage />} />
+        <Route errorElement={<ErrorPage />}>
+          <Route
+            path="/"
+            element={isAuthenticated ? <Navigate to="/home" /> : <LoginPage />}
+          />
+          <Route element={<AuthGuard />}>
+            <Route element={<DashboardLayout />}>
+              <Route path="/home" element={<HomePage />} />
+              <Route path="/total" element={<TotalPage />} />
+              <Route path="/profile/:username" element={<ProfilePage />} />
+              <Route path="/about" element={<AboutPage />} />
+              <Route path="/settings" element={<SettingsPage />} />
+            </Route>
+          </Route>
+          <Route path="*" element={<NotFoundPage />} />
         </Route>
-        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </Router>
   );

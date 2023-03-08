bc055772d249403d62520cc6d6e7a5d39db1fc1c
Yongjun Park <yopark@student.42seoul.kr>
2023-03-08 23:30:30 +0900

    fix: :bug: react-helmet -> react-helmet-async
    react-helmet is not thread-safe

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { HelmetProvider } from 'react-helmet-async';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
+  </React.StrictMode>,
 );
--- app/src/pages/Error.tsx
+++ app/src/pages/Error.tsx
@@ -4,15 +4,15 @@ import { isRouteErrorResponse, useRouteError } from 'react-router-dom';
 export const ErrorPage = () => {
   const error = useRouteError();
 
-  if (isRouteErrorResponse(error)) {
-    return (
-      <>
-        <Helmet>
-          <title>{error.statusText} | 42Stat</title>
-        </Helmet>
-        {error.status} {error.statusText}
-      </>
-    );
+  if (!isRouteErrorResponse(error)) {
+    return <></>;
   }
-  return <></>;
+  return (
+    <>
+      <Helmet>
+        <title>{error.statusText} | 42Stat</title>
+      </Helmet>
+      {error.status} {error.statusText}
+    </>
+  );
 };

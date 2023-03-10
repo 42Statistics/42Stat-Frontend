0463815977a1edcf3fe67e2bd6aadd2ad4242582
Yongjun Park <yopark@student.42seoul.kr>
2023-03-10 14:00:25 +0900

    feat: :sparkles: Apollo 연동

--- app/src/pages/Home.tsx
+++ app/src/pages/Home.tsx
@@ -1,3 +1,34 @@
+import { gql } from '@/__generated__';
+import { useQuery } from '@apollo/client';
+
+const GET_USER = gql(`
+  query GetUser($id: Int!) {
+    user(id: $id) {
+      id
+      login
+    }
+  }
+`);
+
 export const HomePage = () => {
+  const { loading, error, data } = useQuery(GET_USER, {
+    variables: {
+      id: 99947,
+    },
+  });
+  if (loading) {
+    return <h1>loading...</h1>;
+  }
+
+  if (error) {
+    return <h1>{error.message}</h1>;
+  }
+
+  if (!data) {
+    return <h1>user not found</h1>;
+  }
+
+  console.log(data);
+
   return <>HomePage</>;
 };

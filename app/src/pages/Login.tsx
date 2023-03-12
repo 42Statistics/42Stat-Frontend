4d6d04d44592e74f97d1527d6e2d13c238893e33
Yongjun Park <yopark@student.42seoul.kr>
2023-03-12 15:57:16 +0900

    feat: :sparkles: Google 로그인 버튼
    using @react-oauth/google

--- app/src/pages/Login.tsx
+++ app/src/pages/Login.tsx
@@ -1,3 +1,24 @@
+import styled from '@emotion/styled';
+import { GoogleLogin } from '@react-oauth/google';
+
 export const LoginPage = () => {
-  return <>LoginPage</>;
+  return (
+    <LoginPageLayout>
+      <p>
+        42Stat은 42 Intra API를 이용하여 42서울 교육생들의 학습 데이터를
+        분석하여 표시해주는 서비스입니다. 평소 궁금했던 부분들을 42Stat과 함께
+        알아보세요
+      </p>
+      <GoogleLogin
+        onSuccess={(credentialResponse) => {
+          console.log(credentialResponse);
+        }}
+        onError={() => {
+          console.log('Login Failed');
+        }}
+      />
+    </LoginPageLayout>
+  );
 };
+
+const LoginPageLayout = styled('div')``;
--- app/src/vite-env.d.ts
+++ app/src/vite-env.d.ts
@@ -2,6 +2,7 @@
 
 interface ImportMetaEnv {
   readonly VITE_BACKEND_GRAPHQL_ENDPOINT: string;
+  readonly VITE_GAPI_CLIENT_ID: string;
 }
 
 interface ImportMeta {

30d187f6a3ae192e068d3d8a8c37035fdc9179df
Yongjun Park <yopark@student.42seoul.kr>
2023-03-13 21:36:22 +0900

    fix: :bug: emotion theme

--- app/src/styles/global.tsx
+++ app/src/styles/global.tsx
@@ -1,23 +1,28 @@
 import { css } from '@emotion/react';
-import { theme } from './theme';
 
 export const global = css`
   @font-face {
     font-family: 'Noto Sans KR';
+    src: url('fonts/NotoSansKR-Light.otf');
+    font-weight: 300;
+  }
+
+  @font-face {
+    font-family: 'Noto Sans KR';
     src: url('fonts/NotoSansKR-Regular.otf');
-    font-weight: normal;
+    font-weight: 400;
   }
 
   @font-face {
     font-family: 'Noto Sans KR';
     src: url('fonts/NotoSansKR-Medium.otf');
-    font-weight: medium;
+    font-weight: 500;
   }
 
   @font-face {
     font-family: 'Noto Sans KR';
     src: url('fonts/NotoSansKR-Bold.otf');
-    font-weight: bold;
+    font-weight: 700;
   }
 
   html,
@@ -25,10 +30,14 @@ export const global = css`
   #root {
     width: 100%;
     height: 100%;
-    background-color: ${theme.colors.background};
   }
 
   body {
     font-family: 'Noto Sans KR', sans-serif;
+    background-color: #e4e3ea; // FIXME: Global이라 theme 적용 방법을 모르겠다.
+  }
+
+  * {
+    box-sizing: border-box;
   }
 `;

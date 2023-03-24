ae5e1473d30387521ed194cf9353572aeb92f7eb
Yongjun Park <yopark@student.42seoul.kr>
2023-03-24 16:23:04 +0900

    refactor: :recycle: 각 Page에 해당하는 Layout을 밖으로

--- app/src/components/layouts/MainLayout.tsx
+++ app/src/components/layouts/MainLayout.tsx
@@ -1,6 +1,7 @@
 import { Divider } from '@/styles/components';
 import { HStack, VStack } from '@/styles/components/Stack';
 import { Desktop, Tablet, Mobile } from '@/styles/responsive';
+import styled from '@emotion/styled';
 import { Outlet } from 'react-router-dom';
 import { DesktopHeader, TabletHeader, MobileHeader } from '../elements/Header';
 import { DesktopNavBar, TabletNavBar, MobileNavBar } from '../elements/NavBar';
@@ -9,31 +10,46 @@ export const MainLayout = () => {
   return (
     <>
       <Desktop>
-        <HStack>
+        <HStack w="100%" h="100%">
           <DesktopNavBar />
           <VStack w="100%" h="100%" css={{ marginLeft: '24rem' }}>
             <DesktopHeader />
-            <Outlet />
+            <PageLayout>
+              <Outlet />
+            </PageLayout>
           </VStack>
         </HStack>
       </Desktop>
       <Tablet>
-        <HStack>
+        <HStack w="100%" h="100%">
           <TabletNavBar />
           <VStack w="100%" h="100%">
             <TabletHeader />
-            <Outlet />
+            <PageLayout>
+              <Outlet />
+            </PageLayout>
           </VStack>
         </HStack>
       </Tablet>
       <Mobile>
-        <VStack w="100%" spacing="2rem">
+        <VStack w="100%" h="100%" spacing="2rem">
           <MobileHeader />
           <Divider />
-          <Outlet />
+          <PageLayout>
+            <Outlet />
+          </PageLayout>
         </VStack>
         <MobileNavBar />
       </Mobile>
     </>
   );
 };
+
+const PageLayout = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+`;

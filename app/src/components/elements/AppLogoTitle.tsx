44c000af766c99d315deffe812c54f9bd76d0d38
Yongjun Park <yopark@student.42seoul.kr>
2023-03-27 22:36:04 +0900

    fix: :bug: Safari에서는 svg의 width로 rem을 인식하지 못함

--- app/src/components/elements/AppLogoTitle.tsx
+++ app/src/components/elements/AppLogoTitle.tsx
@@ -7,11 +7,12 @@ type AppLogoTitleProps = {
   size: AppLogoSize;
 };
 
+// FIXME: Safari에서는 svg의 width로 rem을 넣으면 에러가 발생한다.
 export const AppLogoTitle = ({ size }: AppLogoTitleProps) => {
   return (
     <HStack spacing={size === 'sm' ? '0.6rem' : '1.2rem'}>
-      <AppLogoSvg width={size === 'sm' ? '2.8rem' : '3.2rem'} />
-      <AppTitleSvg width={size === 'sm' ? '10rem' : '13.5rem'} />
+      <AppLogoSvg width={size === 'sm' ? '28px' : '32px'} />
+      <AppTitleSvg width={size === 'sm' ? '100px' : '135px'} />
     </HStack>
   );
 };

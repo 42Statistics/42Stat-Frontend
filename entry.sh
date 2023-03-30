d7e9e3c88bea675062b5e20d685f48ab526843cf
Yongjun Park <yopark@student.42seoul.kr>
2023-03-30 16:55:32 +0900

    fix: :bug: pnpm compile을 컨테이너 안에서 실행할 수 없습니다

--- entry.sh
+++ entry.sh
@@ -1,1 +1,3 @@
-pnpm i && pnpm compile && pnpm run dev
+# FIXME: pnpm compile을 container 안에서 하면 에러가 발생합니다.
+# pnpm compile을 넣기 위해서는, docker-compose로 frontend와 backend를 동시에 돌리고, backend:3000/graphql을 대상으로 코드를 실행해야 합니다.
+pnpm i && pnpm run dev

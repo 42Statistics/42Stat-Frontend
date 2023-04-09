c3a05874b9e886b087b316643bd0aa06d8522df1
Yongjun Park <yopark@student.42seoul.kr>
2023-04-10 04:12:03 +0900

    feat(init): :sparkles: ErrorPage

--- app/src/routes/AppRoutes.tsx
+++ app/src/routes/AppRoutes.tsx
@@ -1,10 +1,9 @@
-import { NotFoundPage } from '@/pages/404';
+import { NotFoundPage } from '@/pages/ErrorPage';
 import { LoginPage } from '@/pages/LoginPage';
 import { isAuthenticatedAtom } from '@/utils/atoms/isAuthenticatedAtom';
 import { useAtomValue } from 'jotai';
 import { Navigate, useRoutes } from 'react-router-dom';
 import { protectedRoutes } from './protectedRoutes';
-// import { ErrorPage } from '@/pages/Error';
 
 // TODO: <Route errorElement={<ErrorPage />} /> 파트 추가
 export const AppRoutes = () => {

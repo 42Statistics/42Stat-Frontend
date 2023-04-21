50ed4a46659d3b7432ba4a2e1a7adf9335bbebed
Yongjun Park <yopark@student.42seoul.kr>
2023-04-22 04:00:13 +0900

    fix: :bug: Apollo Caching warning

--- app/src/providers/ApolloProvider.tsx
+++ app/src/providers/ApolloProvider.tsx
@@ -2,7 +2,22 @@ import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
   uri: import.meta.env.VITE_BACKEND_GRAPHQL_ENDPOINT,
-  cache: new InMemoryCache(),
+  cache: new InMemoryCache({
+    typePolicies: {
+      Home: {
+        merge: true,
+      },
+      Total: {
+        merge: true,
+      },
+      PersonalGeneral: {
+        merge: true,
+      },
+      PersonalEval: {
+        merge: true,
+      },
+    },
+  }),
 });
 
 const Provider = ({ children }: React.PropsWithChildren) => {

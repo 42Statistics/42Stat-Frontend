68561e909ad6824a4f873561c9b8534aaa24acda
Yongjun Park <yopark@student.42seoul.kr>
2023-04-29 15:54:06 +0900

    refactor(naming): :recycle: Profile(General/Eval)Page -> Profile(General/Eval)Tab

--- app/src/pages/ProfileEvalPage/ProfileEvalPage.tsx
+++
@@ -1,6 +0,0 @@
-import { Dashboard } from '@/components/templates/Dashboard';
-import { useProfileEvalPageDashboard } from './hooks';
-
-export const ProfileEvalPage = () => {
-  return <Dashboard {...useProfileEvalPageDashboard()} />;
-};
--- app/src/pages/ProfileGeneralPage/ProfileGeneralPage.tsx
+++
@@ -1,6 +0,0 @@
-import { Dashboard } from '@/components/templates/Dashboard';
-import { useProfileGeneralPageDashboard } from './hooks';
-
-export const ProfileGeneralPage = () => {
-  return <Dashboard {...useProfileGeneralPageDashboard()} />;
-};

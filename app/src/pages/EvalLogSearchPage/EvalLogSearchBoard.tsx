2dc317ca19b615bace46fd17e33700dea7a51c3e
Yongjun Park <yopark@student.42seoul.kr>
2023-05-04 04:06:09 +0900

    fix: :bug: 빈 파일로 인한 build error

--- app/src/pages/EvalLogSearchPage/EvalLogSearchBoard.tsx
+++ app/src/pages/EvalLogSearchPage/EvalLogSearchBoard.tsx
@@ -1,46 +1,48 @@
-import { GetEvalLogsQuery } from '@/__generated__/graphql';
-import { Loader, Text } from '@/components/common';
-import {
-  ApolloBadRequest,
-  ApolloNotFound,
-} from '@/components/elements/DashboardContentView';
-import { ApolloError } from '@apollo/client';
-import { EvalLogList } from './EvalLogList';
-import { PageMoveArrow } from './PageMoveArrow';
+// import { GetEvalLogsQuery } from '@/__generated__/graphql';
+// import { Loader, Text } from '@/components/common';
+// import {
+//   ApolloBadRequest,
+//   ApolloNotFound,
+// } from '@/components/elements/DashboardContentView';
+// import { ApolloError } from '@apollo/client';
+// import { EvalLogList } from './EvalLogList';
+// import { PageMoveArrow } from './PageMoveArrow';
 
-type EvalLogSearchBoardProps = {
-  loading: boolean;
-  error: ApolloError | undefined;
-  data: GetEvalLogsQuery | undefined;
-  pageNumber: number;
-  setPageNumber: React.Dispatch<React.SetStateAction<number>>;
-};
+// type EvalLogSearchBoardProps = {
+//   loading: boolean;
+//   error: ApolloError | undefined;
+//   data: GetEvalLogsQuery | undefined;
+//   pageNumber: number;
+//   setPageNumber: React.Dispatch<React.SetStateAction<number>>;
+// };
 
-export const EvalLogSearchBoard = ({
-  loading,
-  error,
-  data,
-  pageNumber,
-  setPageNumber,
-}: EvalLogSearchBoardProps) => {
-  if (loading) return <Loader />;
-  if (error) return <ApolloBadRequest msg={error.message} />;
-  if (!data) return <ApolloNotFound />;
-  if (data.getEvalLogs.length === 0) return <Text>검색 결과가 없습니다.</Text>;
+// export const EvalLogSearchBoard = ({
+//   loading,
+//   error,
+//   data,
+//   pageNumber,
+//   setPageNumber,
+// }: EvalLogSearchBoardProps) => {
+//   if (loading) return <Loader />;
+//   if (error) return <ApolloBadRequest msg={error.message} />;
+//   if (!data) return <ApolloNotFound />;
+//   if (data.getEvalLogs.length === 0) return <Text>검색 결과가 없습니다.</Text>;
 
-  return (
-    <>
-      <EvalLogList list={data.getEvalLogs} />
-      <PageMoveArrow
-        handleDecrease={() => {
-          setPageNumber((cur) => Math.max(cur - 1, 1));
-        }}
-        handleIncrease={() => {
-          setPageNumber((cur) => Math.min(cur + 1, 42));
-        }}
-        pageNumber={pageNumber}
-        maxPageNumber={42}
-      />
-    </>
-  );
-};
+//   return (
+//     <>
+//       <EvalLogList list={data.getEvalLogs} />
+//       <PageMoveArrow
+//         handleDecrease={() => {
+//           setPageNumber((cur) => Math.max(cur - 1, 1));
+//         }}
+//         handleIncrease={() => {
+//           setPageNumber((cur) => Math.min(cur + 1, 42));
+//         }}
+//         pageNumber={pageNumber}
+//         maxPageNumber={42}
+//       />
+//     </>
+//   );
+// };
+
+export {};
--- app/src/pages/EvalLogSearchPage/EvalLogSearchHeader.tsx
+++ app/src/pages/EvalLogSearchPage/EvalLogSearchHeader.tsx
@@ -1,78 +1,80 @@
-import {
-  Button,
-  HStack,
-  Input,
-  PrimaryText,
-  Spacer,
-  VStack,
-} from '@/components/common';
-import styled from '@emotion/styled';
-import { SubmitHandler, useForm } from 'react-hook-form';
-import { FormValue } from './EvalLogSearchPage';
+// import {
+//   Button,
+//   HStack,
+//   Input,
+//   PrimaryText,
+//   Spacer,
+//   VStack,
+// } from '@/components/common';
+// import styled from '@emotion/styled';
+// import { SubmitHandler, useForm } from 'react-hook-form';
+// import { FormValue } from './EvalLogSearchPage';
 
-type EvalLogSearchHeaderProps = {
-  formValue: FormValue;
-  onSubmit: SubmitHandler<FormValue>;
-};
+// type EvalLogSearchHeaderProps = {
+//   formValue: FormValue;
+//   onSubmit: SubmitHandler<FormValue>;
+// };
 
-export const EvalLogSearchHeader = ({
-  formValue,
-  onSubmit,
-}: EvalLogSearchHeaderProps) => {
-  const { register, handleSubmit } = useForm<FormValue>({
-    defaultValues: formValue,
-  });
+// export const EvalLogSearchHeader = ({
+//   formValue,
+//   onSubmit,
+// }: EvalLogSearchHeaderProps) => {
+//   const { register, handleSubmit } = useForm<FormValue>({
+//     defaultValues: formValue,
+//   });
 
-  return (
-    <VStack as="form" w="100%" spacing="2rem" onSubmit={handleSubmit(onSubmit)}>
-      <HStack as="ul" w="100%" spacing="2rem" justify="start" wrap="wrap">
-        <HStack as="li" spacing="1rem">
-          <PrimaryText>과제명</PrimaryText>
-          <EvalLogSearchInput {...register('projectName')} />
-        </HStack>
-        <HStack as="li" spacing="1rem">
-          <PrimaryText>FROM</PrimaryText>
-          <EvalLogSearchInput {...register('corrector')} />
-        </HStack>
-        <HStack as="li" spacing="1rem">
-          <PrimaryText>TO</PrimaryText>
-          <EvalLogSearchInput {...register('corrected')} />
-        </HStack>
-        <HStack as="li" spacing="1rem">
-          <PrimaryText>플래그</PrimaryText>
-          <HStack spacing="2rem">
-            <HStack spacing="1rem">
-              <input
-                type="radio"
-                {...register('outstandingOnly')}
-                value="all"
-                defaultChecked={formValue.outstandingOnly === 'all'}
-              />
-              전체
-            </HStack>
-            <HStack spacing="1rem">
-              <input
-                type="radio"
-                {...register('outstandingOnly')}
-                value="outstanding"
-                defaultChecked={formValue.outstandingOnly === 'outstanding'}
-              />
-              Outstanding
-            </HStack>
-          </HStack>
-        </HStack>
-      </HStack>
-      <HStack w="100%">
-        <Spacer />
-        <Button type="submit" text="검색하기" />
-      </HStack>
-    </VStack>
-  );
-};
+//   return (
+//     <VStack as="form" w="100%" spacing="2rem" onSubmit={handleSubmit(onSubmit)}>
+//       <HStack as="ul" w="100%" spacing="2rem" justify="start" wrap="wrap">
+//         <HStack as="li" spacing="1rem">
+//           <PrimaryText>과제명</PrimaryText>
+//           <EvalLogSearchInput {...register('projectName')} />
+//         </HStack>
+//         <HStack as="li" spacing="1rem">
+//           <PrimaryText>FROM</PrimaryText>
+//           <EvalLogSearchInput {...register('corrector')} />
+//         </HStack>
+//         <HStack as="li" spacing="1rem">
+//           <PrimaryText>TO</PrimaryText>
+//           <EvalLogSearchInput {...register('corrected')} />
+//         </HStack>
+//         <HStack as="li" spacing="1rem">
+//           <PrimaryText>플래그</PrimaryText>
+//           <HStack spacing="2rem">
+//             <HStack spacing="1rem">
+//               <input
+//                 type="radio"
+//                 {...register('outstandingOnly')}
+//                 value="all"
+//                 defaultChecked={formValue.outstandingOnly === 'all'}
+//               />
+//               전체
+//             </HStack>
+//             <HStack spacing="1rem">
+//               <input
+//                 type="radio"
+//                 {...register('outstandingOnly')}
+//                 value="outstanding"
+//                 defaultChecked={formValue.outstandingOnly === 'outstanding'}
+//               />
+//               Outstanding
+//             </HStack>
+//           </HStack>
+//         </HStack>
+//       </HStack>
+//       <HStack w="100%">
+//         <Spacer />
+//         <Button type="submit" text="검색하기" />
+//       </HStack>
+//     </VStack>
+//   );
+// };
 
-const EvalLogSearchInput = styled(Input)`
-  all: unset;
-  padding: 0.7rem 2rem;
-  border-radius: 2rem;
-  background-color: ${({ theme }) => theme.colors.primary.light};
-`;
+// const EvalLogSearchInput = styled(Input)`
+//   all: unset;
+//   padding: 0.7rem 2rem;
+//   border-radius: 2rem;
+//   background-color: ${({ theme }) => theme.colors.primary.light};
+// `;
+
+export {};

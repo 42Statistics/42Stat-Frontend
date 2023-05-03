6e29a43525a15ee13f94748625bc03adf462f9ed
Yongjun Park <yopark@student.42seoul.kr>
2023-05-04 06:43:02 +0900

    feat: :lipstick: HomePage 디자인 (시안)

--- app/src/components/common/Button.tsx
+++ app/src/components/common/Button.tsx
@@ -1,24 +1,83 @@
+import { useTheme } from '@emotion/react';
 import styled from '@emotion/styled';
 import { Clickable } from './Clickable';
 
 type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+  size?: 'sm' | 'md';
   text: string;
+  color?: string;
+  bgColor?: string;
+  bg?: string;
 };
 
-export const Button = ({ text, ...propsExceptText }: ButtonProps) => {
+// TODO: size 변경 가능하도록 수정
+export const Button = ({
+  size = 'sm',
+  text,
+  color = useTheme().colors.mono.white,
+  bgColor = useTheme().colors.primary.default,
+  bg = '',
+  ...propsExceptText
+}: ButtonProps) => {
+  const theme = useTheme();
+  const fontSize = size === 'sm' ? theme.fonts.size.body : theme.fonts.size.h3;
+  const padding = size === 'sm' ? '1.1rem 2rem' : '1.3rem 2.4rem';
+
   return (
-    <Clickable element={<ButtonView text={text} />} {...propsExceptText} />
+    <Clickable
+      element={
+        <ButtonView
+          color={color}
+          bgColor={bgColor}
+          bg={bg}
+          text={text}
+          fontSize={fontSize}
+          padding={padding}
+        />
+      }
+      {...propsExceptText}
+    />
   );
 };
 
-const ButtonView = ({ text }: { text: string }) => {
-  return <ButtonViewLayout>{text}</ButtonViewLayout>;
+type ButtonViewProps = {
+  text: string;
+  color: string;
+  bgColor: string;
+  fontSize: string;
+  padding: string;
+  bg: string;
 };
 
-const ButtonViewLayout = styled.div`
-  padding: 0.8rem 2rem;
+const ButtonView = ({
+  color,
+  bgColor,
+  text,
+  fontSize,
+  padding,
+  bg,
+}: ButtonViewProps) => {
+  return (
+    <ButtonViewLayout
+      color={color}
+      bgColor={bgColor}
+      fontSize={fontSize}
+      padding={padding}
+      bg={bg}
+    >
+      {text}
+    </ButtonViewLayout>
+  );
+};
+
+type ButtonViewLayoutProps = Omit<ButtonViewProps, 'text'>;
+
+const ButtonViewLayout = styled.div<ButtonViewLayoutProps>`
   border-radius: 3rem;
-  background-color: ${({ theme }) => theme.colors.primary.default};
-  color: ${({ theme }) => theme.colors.mono.white};
-  font-weight: ${({ theme }) => theme.fonts.weight.medium};
+  background-color: ${({ bgColor }) => bgColor};
+  background: ${({ bg }) => bg};
+  color: ${({ color }) => color};
+  font-weight: ${({ theme }) => theme.fonts.weight.bold};
+  font-size: ${({ fontSize }) => fontSize};
+  padding: ${({ padding }) => padding};
 `;

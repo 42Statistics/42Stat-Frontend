c52cf3b951fa1b0f89c63f84016d6359da90fea4
Yongjun Park <yopark@student.42seoul.kr>
2023-05-19 16:38:50 +0900

    feat: :lipstick: Tab 호버 시 color 변경

--- app/src/components/common/Tab/Tab.tsx
+++ app/src/components/common/Tab/Tab.tsx
@@ -10,12 +10,18 @@ CustomTab.tabsRole = 'Tab';
 const StyledTab = styled(Tab)`
   all: unset;
   padding: 1.4rem 2rem;
-  color: ${({ theme }) => theme.colors.mono.black};
+  color: ${({ theme, selected }) =>
+    !selected ? theme.colors.mono.black : theme.colors.primary.default};
   border-bottom: ${({ theme, selected }) =>
     selected && `3px solid ${theme.colors.primary.default}`};
   background-color: transparent;
   white-space: nowrap;
   cursor: pointer;
+  transition: color 0.2s;
+
+  &:hover {
+    color: ${({ theme }) => theme.colors.primary.default};
+  }
 `;
 
 export { CustomTab as Tab };

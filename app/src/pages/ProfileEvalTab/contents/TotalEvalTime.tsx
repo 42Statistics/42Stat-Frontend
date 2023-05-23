a3bec37dfc0b38e1993b97753e9d2907adcddb9a
Yongjun Park <yopark@student.42seoul.kr>
2023-05-23 16:44:06 +0900

    feat: :sparkles: 누적 평가 시간, 최근 받은 피드백 더미

--- app/src/pages/ProfileEvalTab/contents/TotalEvalTime.tsx
+++ app/src/pages/ProfileEvalTab/contents/TotalEvalTime.tsx
@@ -1,7 +1,15 @@
+import { TextDefault } from '@/components/elements/DashboardContentView/Text';
 import { DashboardContent } from '@/components/templates/Dashboard';
 
 export const TotalEvalTime = () => {
   const title = '누적 평가 시간';
+  const test = 12041; // 분
 
-  return <DashboardContent title={title}></DashboardContent>;
+  return (
+    <DashboardContent title={title}>
+      <TextDefault
+        text={`${Math.floor(test / 60).toLocaleString()}시간 ${test % 60}분`}
+      />
+    </DashboardContent>
+  );
 };

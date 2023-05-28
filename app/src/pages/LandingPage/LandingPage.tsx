198ec91440eaaf303bc22af2771308ff2a59d692
Yongjun Park <yopark@student.42seoul.kr>
2023-05-28 18:12:08 +0900

    feat: :sparkles: accessToken & refreshToken

--- app/src/pages/LandingPage/LandingPage.tsx
+++ app/src/pages/LandingPage/LandingPage.tsx
@@ -1,9 +1,13 @@
 import { Center, Image, VStack, WhiteH1BoldText } from '@/components/common';
 import { AppLogoTitle } from '@/components/elements/AppLogoTitle';
+import { accessTokenAtom } from '@/utils/atoms/accessTokenAtom';
 import { isAuthenticatedAtom } from '@/utils/atoms/isAuthenticatedAtom';
+import { needFtOAuthAtom } from '@/utils/atoms/needFtOAuthAtom';
+import { refreshTokenAtom } from '@/utils/atoms/refreshTokenAtom';
 import { css, useTheme } from '@emotion/react';
-import { GoogleLogin } from '@react-oauth/google';
+import { CredentialResponse, GoogleLogin } from '@react-oauth/google';
 import { useSetAtom } from 'jotai';
+import ky from 'ky';
 import { Helmet } from 'react-helmet-async';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css'; // FIXME: import 말고 Styled Components 방식으로 Slick 적용
@@ -20,13 +24,38 @@ import { CountUp } from 'use-count-up';
 export const LandingPage = () => {
   // TODO: 리더보드 만든 이후 BE 쿼리 위치 정착되면 기준일 HomePage 단에서 전체 다 받아서 description에 주입
   // const { data, loading, error } = useQuery(GET_HOME_PAGE_DESCRIPTION_DATE);
-  const theme = useTheme();
   const setIsAuthenticated = useSetAtom(isAuthenticatedAtom);
+  const setAccessToken = useSetAtom(accessTokenAtom);
+  const setRefreshToken = useSetAtom(refreshTokenAtom);
+  const setNeedFtOAuth = useSetAtom(needFtOAuthAtom);
+
+  const theme = useTheme();
   const textList = [
     '지난주 블랙홀을 마주한 18명',
     '지금까지 블랙홀을 마주한 610명',
     'PUSH_SWAP과 싸우는 32명',
   ];
+
+  const handleGoogleLoginSuccess = async ({
+    clientId,
+    credential,
+  }: CredentialResponse) => {
+    if (!clientId || !credential) {
+      return;
+    }
+    const res = await ky.post(import.meta.env.VITE_GAPI_CALLBACK_URI, {
+      json: { clientId, credential },
+    });
+    const { accessToken, refreshToken, needFtOAuth } = await res.json<{
+      accessToken: string;
+      refreshToken: string;
+      needFtOAuth: boolean;
+    }>();
+    setAccessToken(accessToken);
+    setRefreshToken(refreshToken);
+    setNeedFtOAuth(needFtOAuth);
+    setIsAuthenticated(true);
+  };
 
   return (
     <>
@@ -63,29 +92,19 @@ export const LandingPage = () => {
           </Slider>
         </VStack>
         <Image src="/animated-ship.gif" width="200px" />
-        <GoogleLogin
-          onSuccess={(credentialResponse) => {
-            console.log(credentialResponse);
-          }}
-          onError={() => {
-            console.log('Login Failed');
-          }}
-        />
-        {/* <VStack spacing="2rem">
-          <Text
-              fontSize={theme.fonts.size.h3}
-              fontWeight={theme.fonts.weight.medium}
-              color={theme.colors.mono.white}
-            >
-              🤔 지난 시험의 합격률이 궁금하다면?
-            </Text>
-          <Button
-            size="md"
-            onClick={() => setIsAuthenticated(true)}
-            text="지금 시작하기 🚀"
-            bg="linear-gradient(122deg, rgba(34,160,195,1) 0%, rgba(109,45,253,1) 120%)"
+        <VStack spacing="2rem">
+          {/* <Text
+            fontSize={theme.fonts.size.h3}
+            fontWeight={theme.fonts.weight.medium}
+            color={theme.colors.mono.white}
+          >
+            🤔 지난 시험의 합격률이 궁금하다면?
+          </Text> */}
+          <GoogleLogin
+            onSuccess={handleGoogleLoginSuccess}
+            onError={console.log}
           />
-        </VStack> */}
+        </VStack>
       </VStack>
     </>
   );
@@ -99,67 +118,3 @@ const SliderCenterStyle = css`
     justify-content: center;
   }
 `;
-
-// import { Divider, HStack, VStack, center } from '@/components/common';
-// import { AppLogoTitle } from '@/components/elements/AppLogoTitle';
-// import { AppIntroText } from '@/components/elements/Login/AppIntroText';
-// import { AboveTablet, Mobile } from '@/utils/responsive/Device';
-// import styled from '@emotion/styled';
-// import { GoogleLogin } from '@react-oauth/google';
-// import { Helmet } from 'react-helmet-async';
-
-// export const LoginPage = () => {
-//   return (
-//     <LoginPageLayout>
-//       <Helmet>
-//         <title>42Stat</title>
-//       </Helmet>
-//       <AboveTablet>
-//         <HStack h="40rem" spacing="5vw">
-//           <VStack w="25rem" spacing="5rem">
-//             <AppLogoTitle size="md" />
-//             <AppIntroText />
-//           </VStack>
-//           <Divider orientation="vertical" />
-//           <GoogleLogin
-//             onSuccess={(credentialResponse) => {
-//               console.log(credentialResponse);
-//             }}
-//             onError={() => {
-//               console.log('Login Failed');
-//             }}
-//           />
-//         </HStack>
-//       </AboveTablet>
-//       <Mobile>
-//         <VStack w="25rem" spacing="6rem">
-//           <VStack spacing="2rem">
-//             <AppLogoTitle size="md" />
-//             <AppIntroText />
-//           </VStack>
-//           <Divider />
-//           <GoogleLogin
-//             onSuccess={(credentialResponse) => {
-//               console.log(credentialResponse);
-//             }}
-//             onError={() => {
-//               console.log('Login Failed');
-//             }}
-//           />
-//         </VStack>
-//       </Mobile>
-//     </LoginPageLayout>
-//   );
-// };
-
-// const LoginPageLayout = styled.div`;
-//   ${center}
-//   height: 100%;
-//   background: linear-gradient(
-//     103.55deg,
-//     #cfc2e9 13.93%,
-//     #fee4e2 102.93%,
-//     #ffdb5e 102.94%,
-//     #d9d9d9 102.94%
-//   );
-// `;
--- app/src/vite-env.d.ts
+++ app/src/vite-env.d.ts
@@ -3,6 +3,7 @@
 interface ImportMetaEnv {
   readonly VITE_BACKEND_GRAPHQL_ENDPOINT: string;
   readonly VITE_GAPI_CLIENT_ID: string;
+  readonly VITE_GAPI_CALLBACK_URI: string;
 }
 
 interface ImportMeta {

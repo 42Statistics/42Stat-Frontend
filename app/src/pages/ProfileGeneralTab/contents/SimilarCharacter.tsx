f76428937fc398e88d44c08fd2a2e8c51b7a764f
cmoon <chihun0507@naver.com>
2023-06-05 15:19:33 +0900

    feat: :sparkles: 닮은 포켓몬 페이지 초안

--- app/src/pages/ProfileGeneralTab/contents/SimilarCharacter.tsx
+++ app/src/pages/ProfileGeneralTab/contents/SimilarCharacter.tsx
@@ -1,19 +1,118 @@
 import { H3Text, Image, VStack } from '@/components/common';
 import { DashboardContent } from '@/components/templates/Dashboard';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+/**
+ * 포켓몬 배열
+ * @description
+ * 현재 7x7
+ * Y x X 형태의 배열
+ * 인덱스 값에 변경이 생길경우 getX getY값의 출력범위를 조정해야함
+ */
+const pokemonArray = [
+  ['Pikachu', 'Togepi', 'abra', 'Espeon', 'Lugia', 'Mew', 'Mewtwo'],
+  ['Geodude', 'Eevee', 'Marill', 'Raichu', 'Jolteon', 'Ampharos', 'Zapdos'],
+  ['Onix', 'Dratini', 'Lapras', 'Haunter', 'Gengar', 'Mimikyu', 'Darkrai'],
+  [
+    'Steelix',
+    'Lucario',
+    'Happiny',
+    'Lapras',
+    'Dragonair',
+    'Gyarados',
+    'Kyogre',
+  ],
+  [
+    'Golem',
+    'Snorlax',
+    'Arcanine',
+    'Leafeon',
+    'Togekiss',
+    'Greninja',
+    'Articuno',
+  ],
+  [
+    'Tyranitar',
+    'Lucario',
+    'Charizard',
+    'Sylveon',
+    'Dragonite',
+    'Salamence',
+    'Solgaleo',
+  ],
+  [
+    'Regigigas',
+    'Zacian',
+    'Moltres',
+    'Shaymin',
+    'Jirachi',
+    'Rayquaza',
+    'Arceus',
+  ],
+];
 
 export const SimilarCharacter = () => {
+  const { username } = useParams() as { username: string };
+
   const title = '이 유저를 캐릭터로 표현한다면?';
   const description = '과제 점수, 레벨 증가, 접속 시간, 평가 횟수 기준';
 
-  const characterImgUrl =
-    'https://data1.pokemonkorea.co.kr/newdata/pokedex/full/006601.png';
-  const characterName = '알통몬';
+  const [pokeImg, setPokeImg] = useState(
+    'https://data1.pokemonkorea.co.kr/newdata/pokedex/full/006601.png',
+  );
+  const [pokeName, setPokeName] = useState('unown');
+
+  useEffect(() => {
+    // 인덱싱을 위해 반올림 처리
+    const y = Math.round(getY());
+    const x = Math.round(getX());
+    const fetchURL =
+      'https://pokeapi.co/api/v2/pokemon/' + pokemonArray[y][x].toLowerCase();
+
+    fetch(fetchURL)
+      .then((response) => response.json())
+      .then((data) => {
+        const tmp = data.sprites.other['official-artwork'];
+        const name = data.name;
+        const { front_default, front_shiny } = tmp;
+        /**
+         * username 첫글자가 포켓몬 이름에 포함되어있으면 이로치
+         */
+        if (pokeName.includes(username[0])) {
+          setPokeImg(front_shiny);
+          setPokeName('✨' + name + '✨');
+        } else {
+          setPokeImg(front_default);
+          setPokeName(name);
+        }
+      })
+      .catch((error) => {
+        console.log('데이터를 가져오는 중에 오류가 발생했습니다:', error);
+      });
+  }, [pokeName, username]);
+
+  /**
+   *재능값을 구하는 구하는 로직
+   * @returns 0~6 사이의 재능 값
+   */
+  const getX = () => {
+    return 1;
+  };
+
+  /**
+   *노력값을 구하는 로직
+   * @returns 0~6 사이의 노력 값
+   */
+  const getY = () => {
+    return 2;
+  };
 
   return (
     <DashboardContent title={title} description={description}>
       <VStack h="100%" spacing="3rem">
-        <Image width="200px" src={characterImgUrl} />
-        <H3Text>{characterName}</H3Text>
+        <Image width="200px" src={pokeImg} />
+        <H3Text>{pokeName}</H3Text>
       </VStack>
     </DashboardContent>
   );

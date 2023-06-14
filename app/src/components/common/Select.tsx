8f5f349454ecdc7fedf76c61a211cfdd013897d1
Yongjun Park <yopark@student.42seoul.kr>
2023-06-14 22:48:34 +0900

    ✨ feat: Select

---
+++ app/src/components/common/Select.tsx
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+
+export const Select = styled.select`
+  all: unset;
+  padding: 1rem 2rem;
+  border-radius: 20px;
+  background: url('/down-arrow.svg') no-repeat right 2rem center #f9f9f9;
+  box-shadow: 6px 6px 13px #d2d2d2, -6px -6px 13px #ffffff;
+`;

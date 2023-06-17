b8c947bbb49965bfc4d157fdab51ccb6bf0d6d01
Yongjun Park <yopark@student.42seoul.kr>
2023-06-17 17:34:20 +0900

    🚚 move: Footer device 폴더 분리

--- app/src/components/elements/Footer/index.tsx
+++ app/src/components/elements/Footer/index.tsx
@@ -1,6 +1,6 @@
 import { TabletAndAbove, Mobile } from '@utils/responsive/Device';
-import { TabletAndAboveFooter } from './TabletAndAboveFooter';
-import { MobileFooter } from './MobileFooter';
+import { TabletAndAboveFooter } from './TabletAndAbove';
+import { MobileFooter } from './Mobile';
 
 export const Footer = () => {
   return (

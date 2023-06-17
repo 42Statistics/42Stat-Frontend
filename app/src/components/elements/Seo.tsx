68420ad82c38676a0a227c9361606b9eaa7a4891
Yongjun Park <yopark@student.42seoul.kr>
2023-06-18 01:27:30 +0900

    🔍 seo: og properties

--- app/src/components/elements/Seo.tsx
+++ app/src/components/elements/Seo.tsx
@@ -5,7 +5,6 @@ type SeoProps = {
   description?: string;
 };
 
-// TODO: Seo 추가
 export const Seo = ({
   title,
   description = '42Stat은 42서울의 교육생들을 위한 통계 서비스입니다.',
@@ -14,9 +13,16 @@ export const Seo = ({
     <Helmet>
       <title>{title ? `${title} | 42Stat` : '42Stat'}</title>
       <meta name="description" content={description} />
+      <meta property="og:url" content="https://stat.42seoul.kr" />
       <meta property="og:type" content="website" />
       <meta property="og:title" content={title} />
+      <meta property="og:site_name" content="42Stat" />
       <meta property="og:description" content={description} />
+      {/* TODO: og:image 추가 */}
+      <meta
+        property="og:image"
+        content="https://stat.42seoul.kr/og-image.png"
+      />
       <meta property="twitter:card" content="summary" />
       <meta property="twitter:title" content={title} />
       <meta property="twitter:description" content={description} />

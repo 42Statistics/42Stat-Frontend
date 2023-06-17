341d33e87bb55f4976568db89fa1bbbbd1b401cd
Yongjun Park <yopark@student.42seoul.kr>
2023-06-17 17:14:22 +0900

    🔥 remove: Chart 관련 안 쓰는 파일 삭제

--- app/src/utils/chart/convertToMillion.ts
+++
@@ -1,3 +0,0 @@
-export const convertToMillion = (num: number, fixed = 2) => {
-  return (num / 1000000).toFixed(fixed) + 'M';
-};
--- app/src/utils/chart/index.ts
+++
@@ -1,3 +0,0 @@
-// FIXME: ts는 이렇게 export할 수가 없나?
-export * from './chunkArray';
-export * from './convertToMillion';

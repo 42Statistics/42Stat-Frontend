2bc27ca8a6db502934b03d9a25cf8489c2e915fa
Yongjun Park <yopark@student.42seoul.kr>
2023-06-21 00:48:52 +0900

    🐛 fix: TeamInfo 정렬 기준 원상복구

--- app/src/pages/ProfileGeneralTab/contents/TeamInfo/TeamInfoTable.tsx
+++ app/src/pages/ProfileGeneralTab/contents/TeamInfo/TeamInfoTable.tsx
@@ -28,7 +28,6 @@ export const TeamInfoTable = ({ teams }: TeamInfoTableProps) => {
       <tbody>
         {teams
           .filter(isDefined)
-          .reverse()
           .map(
             ({
               id,

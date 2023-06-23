94ca776751de5e7ef7b3a5c3fd50b605c808beeb
Yongjun Park <yopark@student.42seoul.kr>
2023-06-23 21:33:56 +0900

    ✨ feat: Tab storybook

---
+++ app/src/components/common/Tab/Tab.stories.tsx
@@ -0,0 +1,34 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Tab, TabList } from '.';
+import { Text } from '../Text';
+
+const meta = {
+  title: 'Common/Tab/Tab',
+  component: Tab,
+  tags: ['autodocs'],
+} satisfies Meta<typeof Tab>;
+
+export default meta;
+
+type Story = StoryObj<typeof Tab>;
+
+export const Default: Story = {
+  render: () => (
+    <TabList>
+      <Tab>
+        <Text>일반</Text>
+      </Tab>
+    </TabList>
+  ),
+};
+
+export const Selected: Story = {
+  render: () => (
+    <TabList>
+      <Tab selected>
+        <Text>일반</Text>
+      </Tab>
+    </TabList>
+  ),
+};

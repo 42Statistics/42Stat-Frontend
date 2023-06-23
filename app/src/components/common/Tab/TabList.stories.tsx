0c90e4aa59b962bd7c08832798765dc1cb8a0026
Yongjun Park <yopark@student.42seoul.kr>
2023-06-23 21:34:02 +0900

    ✨ feat: TabList storybook

---
+++ app/src/components/common/Tab/TabList.stories.tsx
@@ -0,0 +1,130 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Tab, TabList } from '.';
+import { Text } from '../Text';
+
+const meta = {
+  title: 'Common/Tab/TabList',
+  component: TabList,
+  tags: ['autodocs'],
+} satisfies Meta<typeof TabList>;
+
+export default meta;
+
+type Story = StoryObj<typeof TabList>;
+
+export const Default: Story = {
+  render: () => (
+    <TabList>
+      <Tab selected>
+        <Text>일반</Text>
+      </Tab>
+      <Tab>
+        <Text>평가</Text>
+      </Tab>
+      <Tab>
+        <Text>라이벌</Text>
+      </Tab>
+    </TabList>
+  ),
+};
+
+export const ManyTabs: Story = {
+  render: () => (
+    <TabList>
+      <Tab selected>
+        <Text>일반</Text>
+      </Tab>
+      <Tab>
+        <Text>평가</Text>
+      </Tab>
+      <Tab>
+        <Text>라이벌</Text>
+      </Tab>
+      <Tab>
+        <Text>라이벌</Text>
+      </Tab>
+      <Tab>
+        <Text>라이벌</Text>
+      </Tab>
+      <Tab>
+        <Text>라이벌</Text>
+      </Tab>
+      <Tab>
+        <Text>라이벌</Text>
+      </Tab>
+      <Tab>
+        <Text>라이벌</Text>
+      </Tab>
+      <Tab>
+        <Text>라이벌</Text>
+      </Tab>
+      <Tab>
+        <Text>라이벌</Text>
+      </Tab>
+      <Tab>
+        <Text>라이벌</Text>
+      </Tab>
+      <Tab>
+        <Text>라이벌</Text>
+      </Tab>
+      <Tab>
+        <Text>라이벌</Text>
+      </Tab>
+      <Tab>
+        <Text>라이벌</Text>
+      </Tab>
+      <Tab>
+        <Text>라이벌</Text>
+      </Tab>
+      <Tab>
+        <Text>라이벌</Text>
+      </Tab>
+      <Tab>
+        <Text>라이벌</Text>
+      </Tab>
+      <Tab>
+        <Text>라이벌</Text>
+      </Tab>
+      <Tab>
+        <Text>라이벌</Text>
+      </Tab>
+      <Tab>
+        <Text>라이벌</Text>
+      </Tab>
+      <Tab>
+        <Text>라이벌</Text>
+      </Tab>
+      <Tab>
+        <Text>라이벌</Text>
+      </Tab>
+      <Tab>
+        <Text>라이벌</Text>
+      </Tab>
+      <Tab>
+        <Text>라이벌</Text>
+      </Tab>
+      <Tab>
+        <Text>라이벌</Text>
+      </Tab>
+      <Tab>
+        <Text>라이벌</Text>
+      </Tab>
+      <Tab>
+        <Text>라이벌</Text>
+      </Tab>
+      <Tab>
+        <Text>라이벌</Text>
+      </Tab>
+      <Tab>
+        <Text>라이벌</Text>
+      </Tab>
+      <Tab>
+        <Text>라이벌</Text>
+      </Tab>
+      <Tab>
+        <Text>라이벌</Text>
+      </Tab>
+    </TabList>
+  ),
+};

7dbf23e4e7de893e0ae12a80a016e3717991cad2
Yongjun Park <yopark@student.42seoul.kr>
2023-06-25 23:36:58 +0900

    🐛 fix: 탭 간 앞으로 가기, 뒤로 가기 안 되는 현상

--- app/src/pages/LeaderBoardPage/index.tsx
+++ app/src/pages/LeaderBoardPage/index.tsx
@@ -4,7 +4,10 @@ import { Seo } from '@components/elements/Seo';
 import styled from '@emotion/styled';
 import { withFooter } from '@hoc/withFooter';
 import { withHead } from '@hoc/withHead';
-import { useState } from 'react';
+import { ROUTES } from '@routes/ROUTES';
+import { history } from '@utils/history';
+import { useEffect, useState } from 'react';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 import {
   LeaderboardCoalitionScoreTab,
   LeaderboardEvalCountTab,
@@ -20,32 +23,73 @@ type LeaderBoardTabNames =
 
 // waterfall 방지를 위해 Tab은 lazy loading 하지 않겠습니다.
 const LeaderBoardPage = () => {
-  const [selectedTab, setSelectedTab] = useState<LeaderBoardTabNames>('Level');
+  const getSelectedTab = (tab: string | null): LeaderBoardTabNames => {
+    switch (tab) {
+      case 'level':
+        return 'Level';
+      case 'exp_increment':
+        return 'ExpIncrement';
+      case 'coalition_score':
+        return 'CoalitionScore';
+      case 'eval_count':
+        return 'EvalCount';
+      default:
+        return 'Level';
+    }
+  };
+
+  const [searchParams] = useSearchParams();
+  const [selectedTab, setSelectedTab] = useState<LeaderBoardTabNames>(
+    getSelectedTab(searchParams.get('tab')),
+  );
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // 탭 간 앞으로 가기, 뒤로 가기
+    history.listen((location) => {
+      if (location.action === 'POP') {
+        const searchParams = new URLSearchParams(location.location.search);
+        setSelectedTab(getSelectedTab(searchParams.get('tab')));
+      }
+    });
+  }, []);
 
   return (
     <>
       <Tabs>
         <Tab
           selected={selectedTab === 'Level'}
-          onClick={() => setSelectedTab('Level')}
+          onClick={() => {
+            setSelectedTab('Level');
+            navigate(ROUTES.LEADERBOARD + '?tab=level');
+          }}
         >
           레벨 랭킹
         </Tab>
         <Tab
           selected={selectedTab === 'ExpIncrement'}
-          onClick={() => setSelectedTab('ExpIncrement')}
+          onClick={() => {
+            setSelectedTab('ExpIncrement');
+            navigate(ROUTES.LEADERBOARD + '?tab=exp_increment');
+          }}
         >
           경험치 증가량
         </Tab>
         <Tab
           selected={selectedTab === 'CoalitionScore'}
-          onClick={() => setSelectedTab('CoalitionScore')}
+          onClick={() => {
+            setSelectedTab('CoalitionScore');
+            navigate(ROUTES.LEADERBOARD + '?tab=coalition_score');
+          }}
         >
           코알리숑 스코어
         </Tab>
         <Tab
           selected={selectedTab === 'EvalCount'}
-          onClick={() => setSelectedTab('EvalCount')}
+          onClick={() => {
+            setSelectedTab('EvalCount');
+            navigate(ROUTES.LEADERBOARD + '?tab=eval_count');
+          }}
         >
           평가 횟수
         </Tab>
--- app/src/pages/ProfilePage/index.tsx
+++ app/src/pages/ProfilePage/index.tsx
@@ -6,15 +6,42 @@ import { withFooter } from '@hoc/withFooter';
 import { withHead } from '@hoc/withHead';
 import ProfileEvalTab from '@pages/ProfileEvalTab';
 import ProfileGeneralTab from '@pages/ProfileGeneralTab';
-import { useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { history } from '@utils/history';
+import { useEffect, useState } from 'react';
+import { useNavigate, useParams, useSearchParams } from 'react-router-dom';
 import { useProfilePageDashboard } from './hooks/useProfilePageDashboard';
 
 type ProfileTabNames = 'General' | 'Eval';
 
 // waterfall 방지를 위해 Tab은 lazy loading 하지 않겠습니다.
 export const ProfilePage = () => {
-  const [selectedTab, setSelectedTab] = useState<ProfileTabNames>('General');
+  const getSelectedTab = (tab: string | null): ProfileTabNames => {
+    switch (tab) {
+      case 'general':
+        return 'General';
+      case 'eval':
+        return 'Eval';
+      default:
+        return 'General';
+    }
+  };
+
+  const [searchParams] = useSearchParams();
+  const [selectedTab, setSelectedTab] = useState<ProfileTabNames>(
+    getSelectedTab(searchParams.get('tab')),
+  );
+  const navigate = useNavigate();
+  const { username } = useParams() as { username: string };
+
+  useEffect(() => {
+    // 탭 간 앞으로 가기, 뒤로 가기
+    history.listen((location) => {
+      if (location.action === 'POP') {
+        const searchParams = new URLSearchParams(location.location.search);
+        setSelectedTab(getSelectedTab(searchParams.get('tab')));
+      }
+    });
+  }, []);
 
   return (
     <VStack w="100%" spacing="2rem">
@@ -22,13 +49,19 @@ export const ProfilePage = () => {
       <Tabs>
         <Tab
           selected={selectedTab === 'General'}
-          onClick={() => setSelectedTab('General')}
+          onClick={() => {
+            setSelectedTab('General');
+            navigate(`/profile/${username}?tab=general`);
+          }}
         >
           일반
         </Tab>
         <Tab
           selected={selectedTab === 'Eval'}
-          onClick={() => setSelectedTab('Eval')}
+          onClick={() => {
+            setSelectedTab('Eval');
+            navigate(`/profile/${username}?tab=eval`);
+          }}
         >
           평가
         </Tab>

4ef7f102d504e663ed9323368ede417619e5d2af
Yongjun Park <yopark@student.42seoul.kr>
2023-06-26 18:14:55 +0900

    ✨ feat: 닮은 캐릭터 연동 (로직 미구현)

--- app/src/pages/ProfileGeneralTab/contents/SimilarCharacter.tsx
+++ app/src/pages/ProfileGeneralTab/contents/SimilarCharacter.tsx
@@ -1,4 +1,5 @@
 import { gql } from '@/__generated__';
+import marvin from '@/assets/marvin-depressed.gif';
 import { H3Text, Image, Loader, VStack } from '@/components/common';
 import { DashboardContent } from '@/components/templates/DashboardContent';
 import { useQuery } from '@apollo/client';
@@ -6,7 +7,7 @@ import {
   ApolloBadRequest,
   ApolloNotFound,
 } from '@components/elements/DashboardContentView/Error';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 /**
@@ -58,12 +59,51 @@ const pokemonArray = [
   ],
 ];
 
-const GET_COALITION = gql(/* GraphQL */ `
-  query getCoalition($login: String!) {
-    getPersonalGeneral(login: $login) {
-      userProfile {
-        coalition {
-          name
+const GET_CHARACTER = gql(/* GraphQL */ `
+  query GetCharacter {
+    getPersonalGeneral {
+      character {
+        effort {
+          logtimeRank {
+            rank
+            value
+            userPreview {
+              id
+            }
+          }
+          evalCountRank {
+            rank
+            value
+          }
+          examTryCount
+          projectTryCount
+        }
+        talent {
+          levelRank {
+            rank
+            value
+          }
+          examOneshotRate {
+            total
+            fields {
+              key
+              value
+            }
+          }
+          projectOneshotRate {
+            total
+            fields {
+              key
+              value
+            }
+          }
+          outstandingRate {
+            total
+            fields {
+              key
+              value
+            }
+          }
         }
       }
     }
@@ -75,84 +115,83 @@ export const SimilarCharacter = () => {
 
   const title = '이 유저를 캐릭터로 표현한다면?';
   const description = '과제 점수, 레벨 증가, 접속 시간, 평가 횟수 기준';
+  const [character, setCharacter] = useState<string>('마빈');
 
-  const [pokeImg, setPokeImg] = useState(
-    'https://data1.pokemonkorea.co.kr/newdata/pokedex/full/006601.png',
-  );
-  const [pokeName, setPokeName] = useState('unown');
+  const { loading, error, data } = useQuery(GET_CHARACTER);
 
-  const { loading, error, data } = useQuery(GET_COALITION, {
-    variables: { login: username },
-  });
+  // const [pokeImg, setPokeImg] = useState(
+  //   'https://data1.pokemonkorea.co.kr/newdata/pokedex/full/006601.png',
+  // );
+  // const [pokeName, setPokeName] = useState('unown');
 
-  /**
-   *재능값을 구하는 구하는 로직
-   * @returns 0~6 사이의 재능 값
-   */
-  const getX = () => {
-    return 1;
-  };
+  // /**
+  //  *재능값을 구하는 구하는 로직
+  //  * @returns 0~6 사이의 재능 값
+  //  */
+  // const getX = () => {
+  //   return 1;
+  // };
 
-  /**
-   *노력값을 구하는 로직
-   * @returns 0~6 사이의 노력 값
-   */
-  const getY = () => {
-    return 2;
-  };
+  // /**
+  //  *노력값을 구하는 로직
+  //  * @returns 0~6 사이의 노력 값
+  //  */
+  // const getY = () => {
+  //   return 2;
+  // };
 
-  useEffect(() => {
-    // 인덱싱을 위해 반올림 처리
-    const y = Math.round(getY());
-    const x = Math.round(getX());
-    let queryName = pokemonArray[y][x].toLowerCase();
+  // useEffect(() => {
+  //   // 인덱싱을 위해 반올림 처리
+  //   const y = Math.round(getY());
+  //   const x = Math.round(getX());
+  //   let queryName = pokemonArray[y][x].toLowerCase();
 
-    // 스타팅 일 경우 건곤감리에 따라 다른 포켓몬 설정
-    if (data) {
-      const { name: coalition } = data.getPersonalGeneral.userProfile.coalition;
-      if (queryName === 'starting') {
-        switch (coalition) {
-          case 'Gun':
-            queryName = 'Pikachu';
-            break;
-          case 'Gon':
-            queryName = 'Bulbasaur';
-            break;
-          case 'Gam':
-            queryName = 'Squirtle';
-            break;
-          case 'Lee':
-            queryName = 'Charmander';
-            break;
-          default:
-            queryName = 'unown';
-        }
-      }
-    }
+  //   // 스타팅 일 경우 건곤감리에 따라 다른 포켓몬 설정
+  //   if (data) {
+  //     const { name: coalition } = data.getPersonalGeneral.userProfile.coalition;
+  //     if (queryName === 'starting') {
+  //       switch (coalition) {
+  //         case 'Gun':
+  //           queryName = 'Pikachu';
+  //           break;
+  //         case 'Gon':
+  //           queryName = 'Bulbasaur';
+  //           break;
+  //         case 'Gam':
+  //           queryName = 'Squirtle';
+  //           break;
+  //         case 'Lee':
+  //           queryName = 'Charmander';
+  //           break;
+  //         default:
+  //           queryName = 'unown';
+  //       }
+  //     }
+  //   }
 
-    const fetchURL = 'https://pokeapi.co/api/v2/pokemon/' + queryName;
+  //   const fetchURL = 'https://pokeapi.co/api/v2/pokemon/' + queryName;
 
-    fetch(fetchURL)
-      .then((response) => response.json())
-      .then((data) => {
-        const tmp = data.sprites.other['official-artwork'];
-        const name = data.name;
-        const { front_default, front_shiny } = tmp;
-        /**
-         * username 첫글자가 포켓몬 이름에 포함되어있으면 이로치
-         */
-        if (pokeName.includes(username[0])) {
-          setPokeImg(front_shiny);
-          setPokeName('✨' + name + '✨');
-        } else {
-          setPokeImg(front_default);
-          setPokeName(name);
-        }
-      })
-      .catch((error) => {
-        console.log('데이터를 가져오는 중에 오류가 발생했습니다:', error);
-      });
-  }, [pokeName, username, data]);
+  //   fetch(fetchURL)
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       const tmp = data.sprites.other['official-artwork'];
+  //       const name = data.name;
+  //       const { front_default, front_shiny } = tmp;
+  //       /**
+  //        * username 첫글자가 포켓몬 이름에 포함되어있으면 이로치
+  //        */
+  //       if (pokeName.includes(username[0])) {
+  //         setPokeImg(front_shiny);
+  //         setPokeName('✨' + name + '✨');
+  //       } else {
+  //         setPokeImg(front_default);
+  //         setPokeName(name);
+  //       }
+  //     })
+  //     .catch((error) => {
+  //       console.log('데이터를 가져오는 중에 오류가 발생했습니다:', error);
+  //     });
+  // }, [pokeName, username, data]);
 
   if (loading)
     return (
@@ -176,12 +215,8 @@ export const SimilarCharacter = () => {
   return (
     <DashboardContent title={title} description={description}>
       <VStack h="100%" spacing="3rem">
-        <Image
-          width="200px"
-          alt="이 유저와 성향이 비슷한 포켓몬"
-          src={pokeImg}
-        />
-        <H3Text>{pokeName}</H3Text>
+        <Image width="200px" src={marvin} />
+        <H3Text>{character}</H3Text>
       </VStack>
     </DashboardContent>
   );

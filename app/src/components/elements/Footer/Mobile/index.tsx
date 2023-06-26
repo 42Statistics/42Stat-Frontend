c9f64767a392d8a87c78a6481b083330b279a8fc
Yongjun Park <yopark@student.42seoul.kr>
2023-06-26 23:43:42 +0900

    ✨ feat: MobileFooter 왼쪽 정렬

--- app/src/components/elements/Footer/Mobile/index.tsx
+++ app/src/components/elements/Footer/Mobile/index.tsx
@@ -1,4 +1,4 @@
-import { HStack, Text, VStack } from '@components/common';
+import { Text, VStack } from '@components/common';
 import { useTheme } from '@emotion/react';
 import styled from '@emotion/styled';
 
@@ -6,20 +6,18 @@ export const MobileFooter = () => {
   const theme = useTheme();
 
   return (
-    <FooterLayout>
-      <VStack>
-        <HStack h="100%">
-          <Text color={theme.colors.mono.gray300} selectable>
-            ©2023 42Stat |&nbsp;
-            <a
-              target="_blank"
-              rel="noreferrer"
-              href="https://github.com/orgs/42Statistics/repositories"
-            >
-              <strong>Github</strong>
-            </a>
-          </Text>
-        </HStack>
+    <Layout>
+      <VStack align="start" spacing="0.5rem">
+        <Text color={theme.colors.mono.gray300} selectable>
+          ©2023 42Stat |&nbsp;
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://github.com/orgs/42Statistics/repositories"
+          >
+            <strong>Github</strong>
+          </a>
+        </Text>
         <Text
           color={theme.colors.mono.gray300}
           selectable
@@ -28,11 +26,11 @@ export const MobileFooter = () => {
           자료에 대한 신뢰 여부는 전적으로 사용자의 책임입니다.
         </Text>
       </VStack>
-    </FooterLayout>
+    </Layout>
   );
 };
 
-const FooterLayout = styled.footer`
+const Layout = styled.footer`
   width: 100%;
-  padding: 4rem 2rem 0 2rem;
+  padding: 4rem 0 0 1rem;
 `;

1a9b9cbc851f03acac2df8758d27caf53275d663
Yongjun Park <yopark@student.42seoul.kr>
2023-06-29 20:42:59 +0900

    ➖ chore: remove rollup-plugin-visualizer

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,23 +1,12 @@
 import basicSsl from '@vitejs/plugin-basic-ssl';
 import react from '@vitejs/plugin-react';
 import * as path from 'path';
-import { visualizer } from 'rollup-plugin-visualizer';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   // todo: ssl
-  plugins: [
-    react(),
-    basicSsl(),
-    visualizer({
-      template: 'treemap',
-      open: true,
-      gzipSize: true,
-      brotliSize: true,
-      filename: 'dist/report.html',
-    }),
-  ],
+  plugins: [react(), basicSsl()],
   server: {
     host: 'frontend',
     port: 8080,
@@ -71,5 +60,5 @@ export default defineConfig({
       { find: '@', replacement: path.resolve(__dirname, 'src') },
     ],
   },
-  envDir: '../env/.env',
+  envDir: '../env',
 });

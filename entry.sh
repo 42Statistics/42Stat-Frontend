ad9d80433d1b244bfdcafd08b255c3cf4a1c6304
Yongjun Park <yopark@student.42seoul.kr>
2023-06-29 20:15:54 +0900

    ♻️ refactor: Dockerfile prod 분리

--- entry.sh
+++ entry.sh
@@ -1,3 +1,1 @@
-# FIXME: pnpm compile을 container 안에서 하면 에러가 발생합니다.
-# pnpm compile을 넣기 위해서는, docker-compose로 frontend와 backend를 동시에 돌리고, backend:3000/graphql을 대상으로 코드를 실행해야 합니다.
 pnpm i && pnpm run dev

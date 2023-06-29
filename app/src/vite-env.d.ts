06a05634804ecab4510cedfaff5ea19a54fd3cb4
Yongjun Park <yopark@student.42seoul.kr>
2023-06-29 19:57:45 +0900

    ✨ feat: env dev & prod 분리에 따른 변경

--- app/src/vite-env.d.ts
+++ app/src/vite-env.d.ts
@@ -3,8 +3,6 @@
 interface ImportMetaEnv {
   readonly VITE_BACKEND_GRAPHQL_ENDPOINT: string;
   readonly VITE_GAPI_CLIENT_ID: string;
-  readonly VITE_BACKEND_AUTH_URI: string;
-  readonly VITE_FTOAUTH_REDIRECT_URI: string;
 }
 
 interface ImportMeta {

1ee217a52fd77f4caccdf3756774b18b48a1c948
Yongjun Park <yopark@student.42seoul.kr>
2023-07-04 01:53:46 +0900

    ♻️ refactor: EvalLogItem 분리

---
+++ app/src/pages/EvalLogSearchPage/EvalLogItem/FlagLabel.tsx
@@ -0,0 +1,19 @@
+import { EvalLogLabel, EvalLogLabelType } from '@components/common/Label';
+import { useTheme } from '@emotion/react';
+import { snakeCaseFormatter } from '@utils/formatters';
+
+type FlagLabelProps = {
+  name: string;
+  isPositive: boolean;
+};
+
+export const FlagLabel = ({ name, isPositive }: FlagLabelProps) => {
+  const theme = useTheme();
+  const type: EvalLogLabelType = isPositive ? 'positive' : 'negative';
+
+  return (
+    <EvalLogLabel type={type} fontWeight={theme.fonts.weight.bold}>
+      {snakeCaseFormatter(name)}
+    </EvalLogLabel>
+  );
+};

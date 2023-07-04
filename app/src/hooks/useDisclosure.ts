ccbbf82a7e847cea238cb50f659ac9cd0662439e
Yongjun Park <yopark@student.42seoul.kr>
2023-07-04 16:50:22 +0900

    🐛 fix: useDisclousure onClose가 계속 호출되는 현상

--- app/src/hooks/useDisclosure.ts
+++ app/src/hooks/useDisclosure.ts
@@ -1,11 +1,11 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 
 export const useDisclosure = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
-  const onOpen = () => setIsOpen(true);
-  const onClose = () => setIsOpen(false);
-  const onToggle = () => setIsOpen((isOpen) => !isOpen);
+  const onOpen = useCallback(() => setIsOpen(true), []);
+  const onClose = useCallback(() => setIsOpen(false), []);
+  const onToggle = useCallback(() => setIsOpen((isOpen) => !isOpen), []);
 
   return { isOpen, onOpen, onClose, onToggle };
 };

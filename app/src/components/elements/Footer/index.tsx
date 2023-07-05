fa3adce167055f77c5b4d3ecb2c0e1ed8ec8112c
Yongjun Park <yopark@student.42seoul.kr>
2023-07-05 21:27:41 +0900

    💄 design: Footer 간소화

--- app/src/components/elements/Footer/index.tsx
+++ app/src/components/elements/Footer/index.tsx
@@ -1,16 +1,31 @@
-import { TabletAndAbove, Mobile } from '@utils/responsive/Device';
-import { TabletAndAboveFooter } from './TabletAndAbove';
-import { MobileFooter } from './Mobile';
+import { GithubLogoSvg } from '@assets/GithubLogoSvg';
+import { HStack, Text } from '@components/common';
+import { useTheme } from '@emotion/react';
+import styled from '@emotion/styled';
+import { Link } from 'react-router-dom';
 
 export const Footer = () => {
+  const theme = useTheme();
+
   return (
-    <>
-      <TabletAndAbove>
-        <TabletAndAboveFooter />
-      </TabletAndAbove>
-      <Mobile>
-        <MobileFooter />
-      </Mobile>
-    </>
+    <Layout>
+      <HStack spacing="1rem">
+        <Text color={theme.colors.mono.gray300} selectable>
+          ©2023. 42Stat
+        </Text>
+        <Link
+          to="https://github.com/orgs/42Statistics/repositories"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <GithubLogoSvg width="16px" fill={theme.colors.mono.gray300} />
+        </Link>
+      </HStack>
+    </Layout>
   );
 };
+
+const Layout = styled.footer`
+  width: 100%;
+  padding: 10rem 2rem 2rem 2rem;
+`;

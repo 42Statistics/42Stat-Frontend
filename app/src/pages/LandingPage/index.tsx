a20d1d39652b9b486c09312a05919e55c88e43d5
Yongjun Park <yopark@student.42seoul.kr>
2023-07-06 15:20:05 +0900

    🐛 fix: isMounted 대신 @keyframes 사용

--- app/src/pages/LandingPage/index.tsx
+++ app/src/pages/LandingPage/index.tsx
@@ -1,27 +1,20 @@
 import { Divider, HStack, VStack } from '@components/common';
 import { FtLoginButton, GoogleLoginButton } from '@components/common/Button';
 import { AppLogoTitle } from '@components/elements/AppLogoTitle';
-import { useTheme } from '@emotion/react';
+import { keyframes, useTheme } from '@emotion/react';
 import styled from '@emotion/styled';
 import { withHead } from '@hoc/withHead';
-import { useEffect, useState } from 'react';
 import { Introduction } from './Introduction';
 
 const LandingPage = () => {
   const theme = useTheme();
-
-  const [isMounted, setIsMounted] = useState<boolean>(false);
-
-  useEffect(() => {
-    setIsMounted(true);
-  }, []);
 
   return (
     <Layout>
       <VStack spacing="10rem">
         <HStack spacing="2rem" h="25rem">
           <Divider color={theme.colors.mono.white} orientation="vertical" />
-          <Title isMounted={isMounted}>
+          <Title>
             <VStack align="start" spacing="6rem">
               <a href="/">
                 <AppLogoTitle size="sm" color={theme.colors.mono.white} />
@@ -43,13 +36,19 @@ const Layout = styled.div`
   user-select: none;
 `;
 
-type TitleProps = {
-  isMounted: boolean;
-};
-
-const Title = styled.div<TitleProps>`
-  transform: ${({ isMounted }) => !isMounted && 'translateX(-30px)'};
-  opacity: ${({ isMounted }) => !isMounted && 0};
-  transition: all 0.5s ease-in-out;
+const siledInFromLeft = keyframes`
+    0% {
+      transform: translateX(-30px);
+      opacity: 0;
+    }
+    100% {
+      transform: translateX(0);
+      opacity: 1;
+    }
 `;
+
+const Title = styled.div`
+  animation: ${siledInFromLeft} 0.5s ease-in-out;
+`;
+
 export default withHead(LandingPage);

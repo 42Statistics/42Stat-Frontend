129360639234183e5914aa5df29266a835279fc6
Yongjun Park <yopark@student.42seoul.kr>
2023-07-06 12:10:52 +0900

    💄 design: LandingPage 디자인 변경

--- app/src/pages/LandingPage/index.tsx
+++ app/src/pages/LandingPage/index.tsx
@@ -1,30 +1,55 @@
-import animated_ship from '@assets/animated-ship.gif';
-import { Image, StartButton, VStack } from '@components/common';
+import { Divider, HStack, VStack } from '@components/common';
+import { FtLoginButton, GoogleLoginButton } from '@components/common/Button';
 import { AppLogoTitle } from '@components/elements/AppLogoTitle';
 import { useTheme } from '@emotion/react';
+import styled from '@emotion/styled';
 import { withHead } from '@hoc/withHead';
-import { ROUTES } from '@routes/ROUTES';
-import { Link } from 'react-router-dom';
-import 'slick-carousel/slick/slick.css'; // FIXME: import 말고 Styled Components 방식으로 Slick 적용
+import { useEffect, useState } from 'react';
 import { Introduction } from './Introduction';
 
 const LandingPage = () => {
   const theme = useTheme();
 
+  const [isMounted, setIsMounted] = useState<boolean>(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
   return (
-    <VStack h="100%" spacing="10rem">
-      <a href="/">
-        <AppLogoTitle size="sm" color={theme.colors.mono.white} />
-      </a>
-      <Introduction />
-      <Image src={animated_ship} alt="우주를 떠다니는 배" width="200px" />
-      <VStack spacing="2rem">
-        <Link to={ROUTES.LOGIN}>
-          <StartButton>지금 시작하기 🚀</StartButton>
-        </Link>
+    <Layout>
+      <VStack spacing="10rem">
+        <HStack spacing="2rem" h="25rem">
+          <Divider color={theme.colors.mono.white} orientation="vertical" />
+          <Title isMounted={isMounted}>
+            <VStack align="start" spacing="6rem">
+              <a href="/">
+                <AppLogoTitle size="sm" color={theme.colors.mono.white} />
+              </a>
+              <Introduction />
+            </VStack>
+          </Title>
+        </HStack>
+        <VStack spacing="2rem">
+          <FtLoginButton />
+          <GoogleLoginButton />
+        </VStack>
       </VStack>
-    </VStack>
+    </Layout>
   );
 };
 
+const Layout = styled.div`
+  user-select: none;
+`;
+
+type TitleProps = {
+  isMounted: boolean;
+};
+
+const Title = styled.div<TitleProps>`
+  transform: ${({ isMounted }) => !isMounted && 'translateX(-30px)'};
+  opacity: ${({ isMounted }) => !isMounted && 0};
+  transition: all 0.5s ease-in-out;
+`;
 export default withHead(LandingPage);

6d2e7d13ece312f89e4b68cefc17e9a024148ccc
Yongjun Park <yopark@student.42seoul.kr>
2023-07-06 12:11:35 +0900

    💄 design: FtOAuthPage 디자인 변경

--- app/src/pages/FtOAuthPage/index.tsx
+++ app/src/pages/FtOAuthPage/index.tsx
@@ -1,31 +1,34 @@
 import { VStack, WhiteText } from '@components/common';
+import { FtLoginButton } from '@components/common/Button';
 import { AppLogoTitle } from '@components/elements/AppLogoTitle';
 import { useTheme } from '@emotion/react';
 import styled from '@emotion/styled';
 import { withHead } from '@hoc/withHead';
 import { ROUTES } from '@routes/ROUTES';
+import { mq } from '@utils/responsive/mq';
 import { Link } from 'react-router-dom';
-import { FtOAuthLoginBtn } from './FtOAuthLoginBtn';
 
 const FtOAuthPage = () => {
   const theme = useTheme();
 
   return (
-    <FtOAuthPageLayout>
+    <Layout>
       <VStack h="100%" spacing="5rem">
         <Link to={ROUTES.ROOT}>
           <AppLogoTitle size="sm" color={theme.colors.mono.white} />
         </Link>
-        <WhiteText>
-          접속하기 위해서는 가입 후 최초 1회 42 인증이 필요합니다.
-        </WhiteText>
-        <FtOAuthLoginBtn />
+        <WhiteText>접속하기 위해서 42 계정과 연동이 필요합니다. </WhiteText>
+        <FtLoginButton />
       </VStack>
-    </FtOAuthPageLayout>
+    </Layout>
   );
 };
 
-const FtOAuthPageLayout = styled.div`
+const Layout = styled.div`
+  ${mq({
+    width: ['100%', 'auto'],
+    height: ['100%', 'auto'],
+  })}
   background: rgba(0, 0, 0, 0.25);
   box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
   backdrop-filter: blur(5px);

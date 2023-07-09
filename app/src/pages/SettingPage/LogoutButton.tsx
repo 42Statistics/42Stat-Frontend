3ac9e3fe832596bd8105dec85b0fb49e4b70736b
Yongjun Park <yopark@student.42seoul.kr>
2023-07-09 21:36:04 +0900

    ✨ feat: 링크 기반 logout -> 함수로 로직 변경

---
+++ app/src/pages/SettingPage/LogoutButton.tsx
@@ -0,0 +1,40 @@
+import { gql } from '@/__generated__';
+import { useMutation } from '@apollo/client';
+import { Button } from '@components/common';
+import { ROUTES } from '@routes/ROUTES';
+import { removeAccessToken } from '@utils/storage/accessToken';
+import { removeGoogleCredential } from '@utils/storage/googleCredential';
+import { removeRefreshToken } from '@utils/storage/refreshToken';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const LOGOUT = gql(/* GraphQL */ `
+  mutation Logout {
+    logout
+  }
+`);
+
+export const LogoutButton = () => {
+  const [logout, { data }] = useMutation(LOGOUT);
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    logout();
+  };
+
+  useEffect(() => {
+    if (!data) {
+      return;
+    }
+    if (data.logout) {
+      removeAccessToken();
+      removeRefreshToken();
+      removeGoogleCredential();
+      navigate(ROUTES.ROOT);
+    } else {
+      alert('로그아웃에 실패했습니다.'); // TODO: UI
+    }
+  }, [data, navigate]);
+
+  return <Button onClick={handleClick}>로그아웃</Button>;
+};

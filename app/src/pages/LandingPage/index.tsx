07cf60d83c0b253def4faa6d5049f718651e3527
Yongjun Park <yopark@student.42seoul.kr>
2023-07-09 21:19:38 +0900

    💄 design: LandingPage 가운데 정렬

--- app/src/pages/LandingPage/index.tsx
+++ app/src/pages/LandingPage/index.tsx
@@ -1,11 +1,10 @@
-import { Divider, HStack, VStack } from '@components/common';
+import { VStack } from '@components/common';
 import { AppLogoTitle } from '@components/elements/AppLogoTitle';
 import {
   FtLoginButton,
   GoogleLoginButton,
 } from '@components/elements/LoginButton';
-import { keyframes, useTheme } from '@emotion/react';
-import styled from '@emotion/styled';
+import { useTheme } from '@emotion/react';
 import { withHead } from '@hoc/withHead';
 import { Introduction } from './Introduction';
 
@@ -14,17 +13,10 @@ const LandingPage = () => {
 
   return (
     <VStack spacing="10rem">
-      <HStack spacing="2rem" h="25rem">
-        <Divider color={theme.colors.mono.white} orientation="vertical" />
-        <Title>
-          <VStack align="start" spacing="6rem">
-            <a href="/">
-              <AppLogoTitle size="sm" color={theme.colors.mono.white} />
-            </a>
-            <Introduction />
-          </VStack>
-        </Title>
-      </HStack>
+      <a href="/">
+        <AppLogoTitle size="sm" color={theme.colors.mono.white} />
+      </a>
+      <Introduction />
       <VStack spacing="2rem">
         <FtLoginButton />
         <GoogleLoginButton />
@@ -32,20 +24,5 @@ const LandingPage = () => {
     </VStack>
   );
 };
-
-const siledInFromLeft = keyframes`
-    0% {
-      transform: translateX(-30px);
-      opacity: 0;
-    }
-    100% {
-      transform: translateX(0);
-      opacity: 1;
-    }
-`;
-
-const Title = styled.div`
-  animation: ${siledInFromLeft} 0.5s ease-in-out;
-`;
 
 export default withHead(LandingPage);

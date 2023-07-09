c614bbd35f977fd7d3a120929ca5acc0ac3d5c1e
Yongjun Park <yopark@student.42seoul.kr>
2023-07-09 21:27:52 +0900

    ✨ feat: custom google login button

--- app/src/components/elements/LoginButton/GoogleLoginButton.tsx
+++ app/src/components/elements/LoginButton/GoogleLoginButton.tsx
@@ -1,23 +1,117 @@
+import { gql } from '@/__generated__';
+import { GAPI_URL } from '@/constants/GAPI';
+import { useScript } from '@/hooks/useScript';
+import { useMutation } from '@apollo/client';
 import google_logo from '@assets/google-logo.svg';
+import { userAtom } from '@atoms/userAtom';
 import { Image } from '@components/common';
 import { ROUTES } from '@routes/ROUTES';
+import { createFakeGoogleWrapper } from '@utils/createFakeGoogleWrapper';
+import { setAccessToken } from '@utils/storage/accessToken';
+import { setGoogleCredential } from '@utils/storage/googleCredential';
+import { setRefreshToken } from '@utils/storage/refreshToken';
+import { useSetAtom } from 'jotai';
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { LoginButton } from './LoginButton';
 
-export const GoogleLoginButton = () => {
-  const navigate = useNavigate();
+const LOGIN_GOOGLE = gql(/* GraphQL */ `
+  mutation LoginGoogle($google: GoogleLoginInput!) {
+    login(google: $google) {
+      __typename
+      ... on UnauthorizedType {
+        status
+        message
+      }
+      ... on NotFoundType {
+        status
+        message
+      }
+      ... on InternalServerErrorType {
+        status
+        message
+      }
+      ... on SuccessType {
+        status
+        accessToken
+        refreshToken
+        userPreview {
+          id
+          login
+          imgUrl
+          displayname
+        }
+      }
+    }
+  }
+`);
 
-  const handleClick = () => {
-    // FIXME: 실제 로직으로 교체
-    localStorage.setItem('googleauth', 'true');
-    navigate(ROUTES.HOME);
+export const GoogleLoginButton = () => {
+  const status = useScript(GAPI_URL);
+  const [login, { data, loading, error }] = useMutation(LOGIN_GOOGLE);
+  const navigate = useNavigate();
+  const setUser = useSetAtom(userAtom);
+
+  const handleClick = (
+    credentialResponse: google.accounts.id.CredentialResponse,
+  ) => {
+    const { credential } = credentialResponse;
+    setGoogleCredential(credential);
+    const clientId = import.meta.env.VITE_GAPI_CLIENT_ID;
+    login({
+      variables: {
+        google: {
+          clientId,
+          credential,
+        },
+      },
+    });
   };
+
+  useEffect(() => {
+    if (loading || error || !data) {
+      return;
+    }
+    if (data.login.__typename !== 'SuccessType') {
+      console.log(data.login);
+      const { status } = data.login;
+      if (status === 401) {
+        navigate(ROUTES.FT_OAUTH);
+      }
+    } else {
+      const { accessToken, refreshToken, userPreview } = data.login;
+      const { id, login, imgUrl, displayname } = userPreview;
+      setAccessToken(accessToken);
+      setRefreshToken(refreshToken);
+      setUser({
+        id,
+        login,
+        imgUrl,
+        displayname,
+      });
+      navigate(ROUTES.HOME);
+    }
+  }, [data, loading, error, setUser, navigate]);
+
+  if (status !== 'ready')
+    return (
+      <LoginButton
+        logo={<GoogleLogo />}
+        text="Google 계정으로 로그인"
+        onClick={() => {
+          /* pass */
+        }}
+        disabled
+      />
+    );
+
+  const googleButtonWrapper = createFakeGoogleWrapper(handleClick);
 
   return (
     <LoginButton
       logo={<GoogleLogo />}
       text="Google 계정으로 로그인"
-      onClick={handleClick}
+      onClick={() => googleButtonWrapper.click()}
     />
   );
 };

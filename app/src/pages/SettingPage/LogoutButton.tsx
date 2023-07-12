c93bbda3fae4c0429711aecf8d5191ac0d9ebf9e
Yongjun Park <yopark@student.42seoul.kr>
2023-07-12 15:57:23 +0900

    ♻️ refactor: logout 함수 분리

--- app/src/pages/SettingPage/LogoutButton.tsx
+++ app/src/pages/SettingPage/LogoutButton.tsx
@@ -1,38 +1,6 @@
-import { gql } from '@/__generated__';
-import { useMutation } from '@apollo/client';
+import { logout } from '@/services/auth/logout';
 import { Button } from '@components/common';
-import { ROUTES } from '@routes/ROUTES';
-import { removeAccessToken } from '@utils/storage/accessToken';
-import { removeRefreshToken } from '@utils/storage/refreshToken';
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-
-const LOGOUT = gql(/* GraphQL */ `
-  mutation Logout {
-    logout
-  }
-`);
 
 export const LogoutButton = () => {
-  const [logout, { data }] = useMutation(LOGOUT);
-  const navigate = useNavigate();
-
-  const handleClick = () => {
-    logout();
-  };
-
-  useEffect(() => {
-    if (!data) {
-      return;
-    }
-    if (data.logout) {
-      removeAccessToken();
-      removeRefreshToken();
-      navigate(ROUTES.ROOT);
-    } else {
-      alert('로그아웃에 실패했습니다.'); // TODO: UI
-    }
-  }, [data, navigate]);
-
-  return <Button onClick={handleClick}>로그아웃</Button>;
+  return <Button onClick={logout}>로그아웃</Button>;
 };

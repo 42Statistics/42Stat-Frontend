2430315e997919a116ad140c6ff320a5f9f04cd0
Yongjun Park <yopark@student.42seoul.kr>
2023-07-12 22:05:24 +0900

    💄 design: Button

--- app/src/components/common/Button/Button.tsx
+++ app/src/components/common/Button/Button.tsx
@@ -10,20 +10,22 @@ type ButtonProps = {
 };
 
 export const Button = styled(Clickable)<ButtonProps>`
-  border-radius: ${({ theme }) => theme.radius.md};
+  border-radius: ${({ theme }) => theme.radius.sm};
   background-color: ${({ theme }) => theme.colors.primary.default};
   color: ${({ theme }) => theme.colors.mono.white};
   font-weight: ${({ theme }) => theme.fonts.weight.medium};
   font-size: ${({ theme }) => theme.fonts.size.body};
   padding: 1rem 2.2rem;
   user-select: none;
-  transition: all 0.15s ease-in-out;
+  transition: all 0.3s;
 
   &:hover {
-    box-shadow: rgba(0, 0, 0, 0.3) 0px 5px 15px;
+    box-shadow: rgba(0, 0, 0, 0.3) 0px 3px 12px;
+    transform: translateY(-2px);
   }
 
   &:active {
     box-shadow: none;
+    transform: translateY(0px);
   }
 `;

ff4d5fdd5b3f9092bb3833a28897452fa8303dfd
Yongjun Park <yopark@student.42seoul.kr>
2023-07-18 02:19:24 +0900

    ♻️ refactor: Leaderboard

--- app/src/pages/LeaderBoardPage/tabs/LeaderboardExpIncrementTabResult.tsx
+++ app/src/pages/LeaderBoardPage/tabs/LeaderboardExpIncrementTabResult.tsx
@@ -7,7 +7,6 @@ import { ApolloErrorView } from '@components/elements/ApolloErrorView';
 import { ApolloNotFoundView } from '@components/elements/ApolloNotFoundView';
 import { Leaderboard } from '@components/templates/Leaderboard';
 import { LeaderboardTabResultSkeleton } from '@pages/PageSkeletons/LeaderboardTabResultSkeleton';
-import { isDefined } from '@utils/isDefined';
 
 type LeaderboardExpIncrementTabResultProps = {
   result: QueryResult<
@@ -23,30 +22,11 @@ export const LeaderboardExpIncrementTabResult = ({
   if (error) return <ApolloErrorView message={error.message} />;
   if (!data) return <ApolloNotFoundView />;
 
-  const { me, totalRanking } =
-    data.getLeaderboardExpIncrement.byDateTemplate.data;
+  const {
+    me,
+    totalRanking: { nodes },
+  } = data.getLeaderboardExpIncrement.byDateTemplate.data;
   const unit = 'XP';
 
-  const myRanking =
-    me != null
-      ? {
-          id: me.userPreview.id,
-          name: me.userPreview.login,
-          value: me.value,
-          rank: me.rank,
-          imgUrl: me.userPreview.imgUrl,
-        }
-      : null;
-
-  const list = totalRanking.nodes
-    .filter(isDefined)
-    .map(({ userPreview, value, rank }) => ({
-      id: userPreview.id,
-      name: userPreview.login,
-      value: value,
-      rank: rank,
-      imgUrl: userPreview.imgUrl,
-    }));
-
-  return <Leaderboard me={myRanking} list={list} unit={unit} />;
+  return <Leaderboard me={me} list={nodes} unit={unit} />;
 };

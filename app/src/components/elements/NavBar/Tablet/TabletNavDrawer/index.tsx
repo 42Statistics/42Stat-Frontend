f54724e3168af3a9b7690822da01369572b249d5
Yongjun Park <yopark@student.42seoul.kr>
2023-07-19 08:24:03 +0900

    ✨ feat: 단축키

--- app/src/components/elements/NavBar/Tablet/TabletNavDrawer/index.tsx
+++ app/src/components/elements/NavBar/Tablet/TabletNavDrawer/index.tsx
@@ -1,17 +1,32 @@
 import { useDisclosure } from '@/hooks/useDisclosure';
 import { Clickable } from '@components/common';
 import { GiHamburgerMenu } from '@react-icons/all-files/gi/GiHamburgerMenu';
+import { isMacBKeyDown } from '@utils/keyboard';
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import { TabletNavDrawerView } from './TabletNavDrawerView';
 
 export const TabletNavDrawer = () => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { isOpen, onOpen, onClose, onToggle } = useDisclosure();
   const location = useLocation();
 
   useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (isMacBKeyDown(e)) {
+        e.preventDefault();
+        onToggle();
+      }
+    };
+    document.addEventListener('keydown', handleKeyDown);
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [onToggle]);
+
+  useEffect(() => {
     onClose();
-  }, [location, onClose]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [location]);
 
   return (
     <>

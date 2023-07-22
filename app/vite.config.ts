5a70a016a6f294a4a4caf12a76b639e54f3a17b5
Yongjun Park <yopark@student.42seoul.kr>
2023-07-23 03:29:11 +0900

    ➕ chore: install vite-plugin-svgr

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -2,11 +2,12 @@ import basicSsl from '@vitejs/plugin-basic-ssl';
 import react from '@vitejs/plugin-react';
 import * as path from 'path';
 import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   // todo: ssl
-  plugins: [react(), basicSsl()],
+  plugins: [react(), basicSsl(), svgr()],
   server: {
     host: 'frontend',
     port: 8080,

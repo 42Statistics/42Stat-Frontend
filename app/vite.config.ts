b26bb2263878cb0b287b7d33d0da63fbe2b1860a
Yongjun Park <yopark@student.42seoul.kr>
2023-07-24 16:35:37 +0900

    🔥 remove: useless alias path

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -19,48 +19,8 @@ export default defineConfig({
   resolve: {
     alias: [
       {
-        find: '@assets',
-        replacement: path.resolve(__dirname, 'src/assets'),
-      },
-      {
-        find: '@guards',
-        replacement: path.resolve(__dirname, 'src/components/guards'),
-      },
-      {
-        find: '@hoc',
-        replacement: path.resolve(__dirname, 'src/components/hoc'),
-      },
-      {
-        find: '@layouts',
-        replacement: path.resolve(__dirname, 'src/components/layouts'),
-      },
-      {
-        find: '@components',
-        replacement: path.resolve(__dirname, 'src/components'),
-      },
-      {
-        find: '@pages',
-        replacement: path.resolve(__dirname, 'src/pages'),
-      },
-      {
-        find: '@providers',
-        replacement: path.resolve(__dirname, 'src/providers'),
-      },
-      {
-        find: '@routes',
-        replacement: path.resolve(__dirname, 'src/routes'),
-      },
-      {
-        find: '@styles',
-        replacement: path.resolve(__dirname, 'src/styles'),
-      },
-      {
-        find: '@atoms',
-        replacement: path.resolve(__dirname, 'src/utils/jotai/atoms'),
-      },
-      {
-        find: '@utils',
-        replacement: path.resolve(__dirname, 'src/utils'),
+        find: '@core',
+        replacement: path.resolve(__dirname, 'src/@core'),
       },
       {
         find: '@shared',

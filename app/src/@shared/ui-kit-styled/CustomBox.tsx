50719795d5f92f994d8d4cddb3716e59296f002e
Yongjun Park <yopark@student.42seoul.kr>
2023-07-26 16:04:15 +0900

    💄 !DESIGN: CustomBox 스타일 변경

--- app/src/@shared/ui-kit-styled/CustomBox.tsx
+++ app/src/@shared/ui-kit-styled/CustomBox.tsx
@@ -3,12 +3,12 @@ import styled from '@emotion/styled';
 export const CustomBox = styled.div`
   width: 100%;
   height: 100%;
-  border-radius: ${({ theme }) => theme.radius.sm};
+  border-radius: ${({ theme }) => theme.radius.md};
   transition: all 0.2s;
-  border: 1px solid ${({ theme }) => theme.colors.mono.gray50};
   background-color: ${({ theme }) => theme.colors.mono.white};
+  box-shadow: 4px 12px 30px 6px rgba(0, 0, 0, 0.09);
 
   &:hover {
-    border: 1px solid ${({ theme }) => theme.colors.mono.gray100};
+    box-shadow: 4px 12px 30px 6px rgba(0, 0, 0, 0.18);
   }
 `;

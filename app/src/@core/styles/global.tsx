78e14c789e483e1301cf5b425df22bef1ea539f2
Yongjun Park <yopark@student.42seoul.kr>
2023-07-27 14:03:56 +0900

    ⚡️ perf: font subset woff2로 교체

--- app/src/@core/styles/global.tsx
+++ app/src/@core/styles/global.tsx
@@ -2,65 +2,38 @@ import { css } from '@emotion/react';
 
 export const global = () => css`
   @font-face {
-    font-family: 'Pretendard';
-    src: url('/fonts/Pretendard-Thin.woff') format('woff');
+    font-family: Pretendard;
+    src: url('/fonts/Pretendard-Thin.subset.woff2') format('woff2');
     font-weight: 100;
     font-display: swap;
   }
 
-  /* @font-face {
-    font-family: 'Pretendard';
-    src: url('/fonts/Pretendard-ExtraLight.woff') format('woff');
-    font-weight: 200;
-  }
-
   @font-face {
-    font-family: 'Pretendard';
-    src: url('/fonts/Pretendard-Light.woff') format('woff');
-    font-weight: 300;
-  } */
-
-  @font-face {
-    font-family: 'Pretendard';
-    src: url('/fonts/Pretendard-Regular.woff') format('woff');
+    font-family: Pretendard;
+    src: url('/fonts/Pretendard-Regular.subset.woff2') format('woff2');
     font-weight: 400;
     font-display: swap;
   }
 
   @font-face {
-    font-family: 'Pretendard';
-    src: url('/fonts/Pretendard-Medium.woff') format('woff');
+    font-family: Pretendard;
+    src: url('/fonts/Pretendard-Medium.subset.woff2') format('woff2');
     font-weight: 500;
     font-display: swap;
   }
 
-  /* @font-face {
-    font-family: 'Pretendard';
-    src: url('/fonts/Pretendard-SemiBold.woff') format('woff');
-    font-weight: 600;
-  } */
-
   @font-face {
-    font-family: 'Pretendard';
-    src: url('/fonts/Pretendard-Bold.woff') format('woff');
+    font-family: Pretendard;
+    src: url('/fonts/Pretendard-Bold.subset.woff2') format('woff2');
     font-weight: 700;
     font-display: swap;
   }
 
-  /* @font-face {
-    font-family: 'Pretendard';
-    src: url('/fonts/Pretendard-ExtraBold.woff') format('woff');
-    font-weight: 800;
-  } */
-
-  /* @font-face {
-    font-family: 'Pretendard';
-    src: url('/fonts/Pretendard-Black.woff') format('woff');
-    font-weight: 900;
-  } */
-
   html {
-    font-family: 'Pretendard', sans-serif;
+    font-family: 'Pretendard Variable', Pretendard, -apple-system,
+      BlinkMacSystemFont, system-ui, Roboto, 'Helvetica Neue', 'Segoe UI',
+      'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic',
+      'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', sans-serif;
     font-display: fallback; // for UX
     font-size: 62.5%; // 1rem = 10px
   }

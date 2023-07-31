b79dfd6819dfab64d325e8bd57cc8cd6f5bcf6f7
Yongjun Park <yopark@student.42seoul.kr>
2023-07-31 19:17:19 +0900

    🐛 fix: localhost일 때 https -> http 로 교체

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -4,33 +4,57 @@ import * as path from 'path';
 import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
 
+const alias = [
+  {
+    find: '@core',
+    replacement: path.resolve(__dirname, 'src/@core'),
+  },
+  {
+    find: '@shared',
+    replacement: path.resolve(__dirname, 'src/@shared'),
+  },
+  { find: '@', replacement: path.resolve(__dirname, 'src') },
+];
+
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => {
+  if (command === 'serve') {
+    return {
+      plugins: [react(), svgr()],
+      server: {
+        host: 'frontend',
+        port: 8080,
+      },
+      preview: {
+        host: 'localhost',
+        port: 8080,
+      },
+      build: {
+        sourcemap: true,
+      },
+      resolve: {
+        alias,
+      },
+      envDir: './env',
+    };
+  }
   // todo: ssl
-  plugins: [react(), basicSsl(), svgr()],
-  server: {
-    host: 'frontend',
-    port: 8080,
-  },
-  preview: {
-    host: 'localhost',
-    port: 8080,
-  },
-  build: {
-    sourcemap: true,
-  },
-  resolve: {
-    alias: [
-      {
-        find: '@core',
-        replacement: path.resolve(__dirname, 'src/@core'),
-      },
-      {
-        find: '@shared',
-        replacement: path.resolve(__dirname, 'src/@shared'),
-      },
-      { find: '@', replacement: path.resolve(__dirname, 'src') },
-    ],
-  },
-  envDir: './env',
+  return {
+    plugins: [react(), basicSsl(), svgr()],
+    server: {
+      host: 'frontend',
+      port: 8080,
+    },
+    preview: {
+      host: 'localhost',
+      port: 8080,
+    },
+    build: {
+      sourcemap: true,
+    },
+    resolve: {
+      alias,
+    },
+    envDir: './env',
+  };
 });

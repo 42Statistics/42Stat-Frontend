3abe00f4e1ba4a4be3b82a3a7aa8b4589c843266
Yongjun Park <yopark@student.42seoul.kr>
2023-08-07 16:17:38 +0900

    🐛 fix: 전역 모달을 close 시 unmount

--- app/src/@core/providers/ModalProvider.tsx
+++ app/src/@core/providers/ModalProvider.tsx
@@ -3,29 +3,15 @@ import { isSpotlightOpenAtom } from '@core/atoms/isSpotlightOpenAtom';
 import { ReLoginDialog } from '@core/components/Modal/ReLoginDialog';
 import { Spotlight } from '@core/components/Spotlight';
 import { PropsWithReactElementChildren } from '@shared/types/PropsWithChildren';
-import { useAtom } from 'jotai';
+import { useAtomValue } from 'jotai';
 
 const ModalProvider = ({ children }: PropsWithReactElementChildren) => {
-  const [isReLoginDialogOpen, setIsReLoginDialogOpen] = useAtom(
-    isReLoginDialogOpenAtom,
-  );
-  const [isSpotlightOpen, setIsSpotlightOpen] = useAtom(isSpotlightOpenAtom);
-
-  const closeReLoginDialog = () => {
-    setIsReLoginDialogOpen(false);
-  };
-
-  const closeSpotlight = () => {
-    setIsSpotlightOpen(false);
-  };
-
+  const isReLoginDialogOpen = useAtomValue(isReLoginDialogOpenAtom);
+  const isSpotlightOpen = useAtomValue(isSpotlightOpenAtom);
   return (
     <>
-      <ReLoginDialog
-        isOpen={isReLoginDialogOpen}
-        onClose={closeReLoginDialog}
-      />
-      <Spotlight isOpen={isSpotlightOpen} onClose={closeSpotlight} />
+      {isReLoginDialogOpen ? <ReLoginDialog /> : null}
+      {isSpotlightOpen ? <Spotlight /> : null}
       {children}
     </>
   );

5f65f419927fe0d58cf126b965cdd7af2a4e5dc7
Yongjun Park <yopark@student.42seoul.kr>
2023-08-10 23:29:55 +0900

    🐛 fix: refreshToken 만료 시에도 logoutButton 작동하도록

--- app/src/Setting/sections/AccountSection/LogoutButton.tsx
+++ app/src/Setting/sections/AccountSection/LogoutButton.tsx
@@ -1,6 +1,33 @@
-import { logout } from '@shared/services/auth/logout';
+import { useMutation } from '@apollo/client';
+import { gql } from '@shared/__generated__';
+import { ROUTES } from '@shared/constants/ROUTES';
 import { Button } from '@shared/ui-kit';
+import { clearStorage } from '@shared/utils/storage/clearStorage';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const LOGOUT = gql(/* GraphQL */ `
+  mutation Logout {
+    logout
+  }
+`);
 
 export const LogoutButton = () => {
-  return <Button onClick={logout}>로그아웃</Button>;
+  const [logout, { called, loading }] = useMutation(LOGOUT);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!called) {
+      return;
+    }
+    if (loading) {
+      return;
+    }
+
+    // 실패해도 ROOT로 이동
+    clearStorage();
+    navigate(ROUTES.ROOT);
+  }, [called, loading, navigate]);
+
+  return <Button onClick={() => logout()}>로그아웃</Button>;
 };

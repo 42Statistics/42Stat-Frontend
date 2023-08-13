d3c44ee45559f5a267ce4d2babfcdbf17889eaea
Yongjun Park <yopark@student.42seoul.kr>
2023-08-14 02:08:18 +0900

    💄 design: ft redirect 409 alert -> dialog

--- app/src/FtOAuthRedirect/index.tsx
+++ app/src/FtOAuthRedirect/index.tsx
@@ -3,7 +3,8 @@ import { gql } from '@shared/__generated__';
 import { userAtom } from '@shared/atoms/userAtom';
 import { GOOGLE_LOGIN } from '@shared/components/LoginButton';
 import { ROUTES } from '@shared/constants/ROUTES';
-import { Loader } from '@shared/ui-kit';
+import { useDisclosure } from '@shared/hooks/useDisclosure';
+import { AlertDialog, Loader } from '@shared/ui-kit';
 import { setAccessToken } from '@shared/utils/storage/accessToken';
 import {
   getGoogleCredential,
@@ -28,6 +29,8 @@ const FT_LOGIN = gql(/* GraphQL */ `
 const FtOAuthRedirectPage = () => {
   const [searchParams] = useSearchParams();
   const ftCode = searchParams.get('code');
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   const [ftLogin, { data: ftData, loading: ftLoading, error: ftError }] =
     useMutation(FT_LOGIN);
   const [
@@ -93,9 +96,7 @@ const FtOAuthRedirectPage = () => {
         (error) => error.extensions?.status === 409,
       );
       if (isConflict) {
-        alert('이미 다른 구글 계정과 연동되어있습니다.'); // TODO: Modal
-        removeGoogleCredential();
-        navigate(ROUTES.ROOT);
+        onOpen();
       }
       return;
     }
@@ -110,13 +111,28 @@ const FtOAuthRedirectPage = () => {
     setRefreshToken(refreshToken);
     removeGoogleCredential();
     navigate(ROUTES.HOME);
-  }, [googleData, googleLoading, googleError, navigate, setUser]);
+  }, [googleLoading, googleError, googleData]);
 
   if (ftCode === null) {
     return <Navigate to={ROUTES.ROOT} />;
   }
 
-  return <Loader />;
+  return (
+    <>
+      <Loader />
+      <AlertDialog
+        isOpen={isOpen}
+        onClose={onClose}
+        title="계정 연동 실패"
+        description="해당 42 계정은 이미 다른 구글 계정과 연동되어있습니다."
+        confirmText="홈으로 이동"
+        onConfirm={() => {
+          removeGoogleCredential();
+          navigate(ROUTES.ROOT);
+        }}
+      />
+    </>
+  );
 };
 
 export default FtOAuthRedirectPage;

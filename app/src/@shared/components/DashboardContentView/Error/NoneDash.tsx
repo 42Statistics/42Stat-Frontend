4749726cf370c20e80f89c8539974aa895282213
Yongjun Park <yopark@student.42seoul.kr>
2023-08-22 06:50:56 +0900

    fix(dark-mode): NoneDash

--- app/src/@shared/components/DashboardContentView/Error/NoneDash.tsx
+++ app/src/@shared/components/DashboardContentView/Error/NoneDash.tsx
@@ -1,5 +1,10 @@
+import { useTheme } from '@emotion/react';
 import { ReactComponent as MdHorizontalRule } from '@shared/assets/icon/md-horizontal-rule.svg';
 
 export const NoneDash = () => {
-  return <MdHorizontalRule width={20} height={20} />;
+  const theme = useTheme();
+
+  return (
+    <MdHorizontalRule width={20} height={20} fill={theme.colors.mono.black} />
+  );
 };

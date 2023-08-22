9ef6de02b9aaa61295813b764b8fd2da40b52df9
Yongjun Park <yopark@student.42seoul.kr>
2023-08-22 13:55:58 +0900

    fix(dark-mode): FtLogo 다크모드일 때 하얀색

--- app/src/@shared/components/LoginButton/FtLoginButton.tsx
+++ app/src/@shared/components/LoginButton/FtLoginButton.tsx
@@ -1,3 +1,4 @@
+import { useTheme } from '@emotion/react';
 import { ReactComponent as FtLogo } from '@shared/assets/logo/ft-logo.svg';
 import {
   FT_OAUTH_ENDPOINT,
@@ -7,6 +8,7 @@ import { ARIA_LABEL_BUTTON } from '@shared/constants/accessibility/ARIA_LABEL';
 import { LoginButton } from './LoginButton';
 
 export const FtLoginButton = () => {
+  const theme = useTheme();
   const params = new URLSearchParams();
   params.append('client_id', import.meta.env.VITE_FT_OAUTH_CLIENT_ID);
   params.append('redirect_uri', import.meta.env.VITE_FT_OAUTH_REDIRECT_URI);
@@ -15,7 +17,7 @@ export const FtLoginButton = () => {
 
   return (
     <LoginButton
-      logo={<FtLogo width={18} height={18} />}
+      logo={<FtLogo width={18} height={18} fill={theme.colors.mono.black} />}
       text="42 계정으로 로그인"
       ariaLabel={ARIA_LABEL_BUTTON.LOGIN_WITH_42_ACCOUNT}
       onClick={() => {

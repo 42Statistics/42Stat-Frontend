65bd82cf673d8381cbe766f1356cfdcc67f646ae
Yongjun Park <yopark@student.42seoul.kr>
2023-10-04 00:17:16 +0900

    refactor(LeaderboardHeader): left, right 삭제

--- app/src/Leaderboard/components/Leaderboard/LeaderboardHeader.tsx
+++ app/src/Leaderboard/components/Leaderboard/LeaderboardHeader.tsx
@@ -1,20 +1,48 @@
 import styled from '@emotion/styled';
+import { useAtomValue } from 'jotai';
 
+import { leaderboardPromoListAtom } from '@/Leaderboard/atoms/leaderboardPromoListAtom';
+import LeaderboardDateDescriptor from '@/Leaderboard/components/Leaderboard/LeaderboardDateDescriptor';
+import PromoSelectList from '@/Leaderboard/components/PromoSelect/PromoSelectList';
+import { Select, SelectContent, SelectTrigger } from '@shared/ui-kit';
 import { mq } from '@shared/utils/facepaint/mq';
 
 type LeaderboardHeaderProps = {
-  left: React.ReactNode;
-  right: React.ReactNode;
+  currSegmentIndex?: number;
+  currPromo: number | null;
+  onPromoChange: (promo: string | null) => void;
+  start: Date;
+  end: Date;
 };
 
 export default function LeaderboardHeader({
-  left,
-  right,
+  currSegmentIndex,
+  currPromo,
+  onPromoChange,
+  start,
+  end,
 }: LeaderboardHeaderProps) {
+  const promoList = useAtomValue(leaderboardPromoListAtom);
+
+  if (promoList === null) {
+    throw new Error('promoList is null');
+  }
+
   return (
     <Layout>
-      {left}
-      {right}
+      <Select
+        key={currSegmentIndex}
+        width="21rem"
+        onValueChange={onPromoChange}
+        defaultValue={currPromo !== null ? String(currPromo) : undefined}
+        defaultRenderValue={currPromo !== null ? `${currPromo}기` : undefined}
+      >
+        <SelectTrigger placeholder="전체" />
+        <SelectContent maxHeight="20rem">
+          <PromoSelectList list={promoList ?? []} />
+        </SelectContent>
+      </Select>
+      <LeaderboardDateDescriptor start={start} end={end} />
     </Layout>
   );
 }
--- app/src/Leaderboard/pages/Level/index.tsx
+++ app/src/Leaderboard/pages/Level/index.tsx
@@ -3,11 +3,8 @@ import { useAtomValue } from 'jotai';
 import { useSearchParams } from 'react-router-dom';
 
 import { leaderboardArgsAtom } from '@/Leaderboard/atoms/leaderboardArgsAtom';
-import { leaderboardPromoListAtom } from '@/Leaderboard/atoms/leaderboardPromoListAtom';
 import { Leaderboard } from '@/Leaderboard/components/Leaderboard';
-import LeaderboardDateDescriptor from '@/Leaderboard/components/Leaderboard/LeaderboardDateDescriptor';
 import LeaderboardHeader from '@/Leaderboard/components/Leaderboard/LeaderboardHeader';
-import PromoSelectList from '@/Leaderboard/components/PromoSelect/PromoSelectList';
 import { LeaderboardResultSkeleton } from '@/Leaderboard/components/skeletons/LeaderboardResultSkeleton';
 import {
   LEADERBOARD_DEFAULT_OPTIONS,
@@ -19,13 +16,7 @@ import { DateTemplate } from '@shared/__generated__/graphql';
 import { FullPageApolloErrorView } from '@shared/components/ApolloError/FullPageApolloErrorView';
 import { Pagination } from '@shared/components/Pagination';
 import { Seo } from '@shared/components/Seo';
-import {
-  DeferredComponent,
-  Select,
-  SelectContent,
-  SelectTrigger,
-  VStack,
-} from '@shared/ui-kit';
+import { DeferredComponent, VStack } from '@shared/ui-kit';
 import { useDeviceType } from '@shared/utils/react-responsive/useDeviceType';
 
 import { GET_LEADERBOARD_LEVEL } from './queries/getLeaderboardLevel';
@@ -36,14 +27,9 @@ export default function LeaderboardLevelPage() {
   const { PAGE, PROMO } = LEADERBOARD_PARAM_KEYS;
 
   const leaderboardArgs = useAtomValue(leaderboardArgsAtom);
-  const promoList = useAtomValue(leaderboardPromoListAtom);
 
   if (leaderboardArgs === null) {
     throw new Error('leaderboardArgs is null');
-  }
-
-  if (promoList === null) {
-    throw new Error('promoList is null');
   }
 
   const { loading, error, data } = useQuery(GET_LEADERBOARD_LEVEL, {
@@ -114,25 +100,10 @@ export default function LeaderboardLevelPage() {
       <VStack w="100%" spacing="6rem">
         <VStack w="100%" spacing="2rem">
           <LeaderboardHeader
-            left={
-              <Select
-                width="21rem"
-                onValueChange={handlePromoChange}
-                defaultValue={promo !== null ? String(promo) : undefined}
-                defaultRenderValue={promo !== null ? `${promo}기` : undefined}
-              >
-                <SelectTrigger placeholder="전체" />
-                <SelectContent maxHeight="20rem">
-                  <PromoSelectList list={promoList ?? []} />
-                </SelectContent>
-              </Select>
-            }
-            right={
-              <LeaderboardDateDescriptor
-                start={new Date(start)}
-                end={new Date(end)}
-              />
-            }
+            currPromo={promo}
+            onPromoChange={handlePromoChange}
+            start={new Date(start)}
+            end={new Date(end)}
           />
           <Leaderboard me={me} list={nodes} fixedNumber={2} />
         </VStack>

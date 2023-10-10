62fb0ca3c16e3a39900aeb2c7bdf5e70c06ce748
Yongjun Park <yopark@student.42seoul.kr>
2023-10-10 15:11:37 +0900

    style(calculator): prettier

--- app/src/@core/components/NavBar/Tablet/TabletNavMenu.tsx
+++ app/src/@core/components/NavBar/Tablet/TabletNavMenu.tsx
@@ -11,7 +11,7 @@ export const TabletNavMenu = () => {
       <TabletNavItem route={NAV_ROUTES.PROFILE} />
       <TabletNavItem route={NAV_ROUTES.LEADERBOARD} />
       <TabletNavItem route={NAV_ROUTES.EVALLOG} />
-			<TabletNavItem route={NAV_ROUTES.CALCULATOR} />
+      <TabletNavItem route={NAV_ROUTES.CALCULATOR} />
       <Spacer />
       <TabletNavItem route={NAV_ROUTES.SETTING} />
     </VStack>

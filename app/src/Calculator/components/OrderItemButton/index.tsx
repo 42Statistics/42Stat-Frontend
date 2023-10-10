7c535d4466f9e56820455bf4ce14155b331938ac
Yongjun Park <yopark@student.42seoul.kr>
2023-10-10 16:40:56 +0900

    feat(calculator): IconButton

--- app/src/Calculator/components/OrderItemButton/index.tsx
+++ app/src/Calculator/components/OrderItemButton/index.tsx
@@ -5,6 +5,7 @@ import type { TableRowList } from '@/Calculator/types/OrderItemButton';
 import { ReactComponent as MdDelete } from '@shared/assets/icon/md-delete.svg';
 import { ReactComponent as MdDown } from '@shared/assets/icon/md-down.svg';
 import { ReactComponent as MdUp } from '@shared/assets/icon/md-up.svg';
+import { IconButton } from '@shared/ui-kit';
 
 export const OrderItemButton = ({
   tableRowList,
@@ -39,30 +40,15 @@ export const OrderItemButton = ({
 
   return (
     <Layout>
-      <ButtonLayout>
-        <MdUp
-          width={15}
-          height={15}
-          fill={theme.colors.mono.black}
-          onClick={handleClickUp}
-        />
-      </ButtonLayout>
-      <ButtonLayout>
-        <MdDown
-          width={15}
-          height={15}
-          fill={theme.colors.mono.black}
-          onClick={handleClickDown}
-        />
-      </ButtonLayout>
-      <ButtonLayout>
-        <MdDelete
-          width={15}
-          height={15}
-          fill={theme.colors.mono.black}
-          onClick={handleClickDelete}
-        />
-      </ButtonLayout>
+      <IconButton onClick={handleClickUp}>
+        <MdUp width={15} height={15} fill={theme.colors.mono.black} />
+      </IconButton>
+      <IconButton onClick={handleClickDown}>
+        <MdDown width={15} height={15} fill={theme.colors.mono.black} />
+      </IconButton>
+      <IconButton onClick={handleClickDelete}>
+        <MdDelete width={15} height={15} fill={theme.colors.mono.black} />
+      </IconButton>
     </Layout>
   );
 };
@@ -83,24 +69,5 @@ const Layout = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-`;
-
-const ButtonLayout = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  padding: 0.5rem 0.5rem;
-  user-select: none;
-  transition: all 0.3s;
-
-  &:hover {
-    box-shadow: rgba(0, 0, 0, 0.3) 0px 3px 12px;
-    transform: translateY(-2px);
-    cursor: pointer;
-  }
-
-  &:active {
-    box-shadow: none;
-    transform: translateY(0px);
-  }
+  gap: 0.3rem;
 `;

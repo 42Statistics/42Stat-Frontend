667d83c211d4e72670dd2e3e85d3d54431b62f95
sungwook <106149304+42sungwook@users.noreply.github.com>
2023-10-13 15:44:44 +0900

    Revert "refactor(calculator): CalculatorInput 컴포넌트 쪼개기"
    This reverts commit 3cb8eb5aa5f29cd53f4ab7fdfbab4096dd566d3a.

--- app/src/Calculator/constants/projectListTitles.ts
+++
@@ -1,8 +0,0 @@
-export const PROJECT_LIST_TITLES = {
-  PROJECT_NAME: '프로젝트명',
-  SCORE: '점수',
-  COALITION_BONUS: '코알리숑 보너스',
-  EXP: '경험치',
-  FINISH_LEVEL: '통과 레벨',
-  BLACKHOLE: '블랙홀',
-} as const;

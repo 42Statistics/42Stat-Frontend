a8d10a9868d1790a47f6c86f5fa835a4d80a9473
Yongjun Park <yopark@student.42seoul.kr>
2023-10-18 20:48:57 +0900

    fix: 최근 쓴 코멘트 0개일 때 처리

--- app/src/Profile/dashboard-contents/Eval/RecentComment.tsx
+++ app/src/Profile/dashboard-contents/Eval/RecentComment.tsx
@@ -6,6 +6,7 @@ import {
   DashboardContentLoading,
   DashboardContentNotFound,
 } from '@shared/components/DashboardContentView/Error';
+import { TextDefault } from '@shared/components/DashboardContentView/Text/TextDefault';
 import { InfoTooltip } from '@shared/components/InfoTooltip';
 import { Text } from '@shared/ui-kit';
 import { useContext } from 'react';
@@ -35,12 +36,23 @@ export const RecentComment = () => {
   const { recentComment } = data.getPersonalEval; // FIXME: null일 수 있음.
 
   return (
-    <DashboardContent
-      title={title}
-      titleRight={<InfoTooltip text="코멘트 : 평가하러 가서 작성한 리뷰" />}
-      type="Scrollable"
-    >
-      <Text>{recentComment}</Text>
-    </DashboardContent>
+    <>
+      {recentComment ? (
+        <DashboardContent
+          title={title}
+          titleRight={<InfoTooltip text="코멘트 : 평가하러 가서 작성한 리뷰" />}
+          type="Scrollable"
+        >
+          <Text>{recentComment}</Text>
+        </DashboardContent>
+      ) : (
+        <DashboardContent
+          title={title}
+          titleRight={<InfoTooltip text="코멘트 : 평가하러 가서 작성한 리뷰" />}
+        >
+          <TextDefault text="평가 기록이 없습니다" />
+        </DashboardContent>
+      )}
+    </>
   );
 };

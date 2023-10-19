9bd876df749c8b5f49332a6fdbedce88c6076672
Yongjun Park <yopark@student.42seoul.kr>
2023-10-19 13:08:22 +0900

    fix: 팀 정보 기록 없을 때 처리

--- app/src/Profile/dashboard-contents/General/TeamInfo/index.tsx
+++ app/src/Profile/dashboard-contents/General/TeamInfo/index.tsx
@@ -2,11 +2,13 @@ import { UserProfileContext } from '@/Profile/contexts/UserProfileContext';
 import { useQuery } from '@apollo/client';
 import styled from '@emotion/styled';
 import { gql } from '@shared/__generated__';
+import { DashboardContent } from '@shared/components/DashboardContent';
 import {
   DashboardContentBadRequest,
   DashboardContentLoading,
   DashboardContentNotFound,
 } from '@shared/components/DashboardContentView/Error';
+import { TextDefault } from '@shared/components/DashboardContentView/Text/TextDefault';
 import { useContext } from 'react';
 import { TeamInfoTable } from './TeamInfoTable';
 
@@ -41,19 +43,29 @@ export const TeamInfo = () => {
 
   const { teams } = data.getPersonalGeneral.teamInfo;
 
+  const title = '팀 정보';
+
   return (
-    <TeamInfoLayout>
-      <div
-        style={{
-          position: 'relative',
-          width: '100%',
-          height: '100%',
-          overflow: 'auto',
-        }}
-      >
-        <TeamInfoTable teams={teams} />
-      </div>
-    </TeamInfoLayout>
+    <>
+      {teams.length === 0 ? (
+        <TeamInfoLayout>
+          <div
+            style={{
+              position: 'relative',
+              width: '100%',
+              height: '100%',
+              overflow: 'auto',
+            }}
+          >
+            <TeamInfoTable teams={teams} />
+          </div>
+        </TeamInfoLayout>
+      ) : (
+        <DashboardContent title={title}>
+          <TextDefault text="프로젝트 신청 기록이 없습니다." />
+        </DashboardContent>
+      )}
+    </>
   );
 };
 

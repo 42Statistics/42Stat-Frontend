bec68547124b781c5bafde05b4dca01ec5701af9
Yongjun Park <yopark@student.42seoul.kr>
2023-10-22 03:36:27 +0900

    refactor(app-router): React Router v6.4 방식으로 교체
    Router 바깥에서 useLocation을 호출할 수 없어 일단 ScrollToTop을 무력화하였습니다.
    어차피 ScrollToTop은 곧 ScrollRestoration을 위해 삭제될 예정이었습니다.
    
    See also #319

--- app/src/@core/providers/ScrollToTop.tsx
+++ app/src/@core/providers/ScrollToTop.tsx
@@ -1,13 +1,10 @@
-import { useEffect } from 'react';
-import { useLocation, useSearchParams } from 'react-router-dom';
-
 import type { PropsWithReactElementChildren } from '@shared/types/PropsWithChildren';
 
 export const ScrollToTop = ({ children }: PropsWithReactElementChildren) => {
-  const location = useLocation();
-  const [searchParams] = useSearchParams();
+  // const location = useLocation();
+  // const [searchParams] = useSearchParams();
 
-  useEffect(() => window.scrollTo(0, 0), [location.pathname, searchParams]);
+  // useEffect(() => window.scrollTo(0, 0), [location.pathname, searchParams]);
 
   return <>{children}</>;
 };

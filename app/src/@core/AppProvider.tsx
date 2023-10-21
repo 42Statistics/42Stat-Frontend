0a6cbacb405350f463a6ba2eec5b46a065cf81fc
Yongjun Park <yopark@student.42seoul.kr>
2023-10-22 03:51:44 +0900

    chore: remove ScrollToTop

--- app/src/@core/AppProvider.tsx
+++ app/src/@core/AppProvider.tsx
@@ -2,7 +2,6 @@ import ApolloProvider from '@core/providers/ApolloProvider';
 import ErrorBoundaryProvider from '@core/providers/ErrorBoundaryProvider';
 import HelmetProvider from '@core/providers/HelmetProvider';
 import ModalProvider from '@core/providers/ModalProvider';
-import { ScrollToTop } from '@core/providers/ScrollToTop';
 import ThemeProvider from '@core/providers/ThemeProvider';
 import type { PropsWithReactElementChildren } from '@shared/types/PropsWithChildren';
 
@@ -12,9 +11,7 @@ export const AppProvider = ({ children }: PropsWithReactElementChildren) => {
       <ApolloProvider>
         <HelmetProvider>
           <ThemeProvider>
-            <ScrollToTop>
-              <ModalProvider>{children}</ModalProvider>
-            </ScrollToTop>
+            <ModalProvider>{children}</ModalProvider>
           </ThemeProvider>
         </HelmetProvider>
       </ApolloProvider>

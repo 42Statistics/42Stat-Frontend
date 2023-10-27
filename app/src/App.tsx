570b601eced0e3b4db62dfc523f1f9cbf12acc05
김성욱 <sungwook@MacAir-2.local>
2023-10-27 14:43:57 +0900

    chore: import 순서 수정

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -5,8 +5,8 @@ import { AppProvider } from '@core/AppProvider';
 import { appRouter } from '@core/appRouter';
 import { global } from '@core/styles/global';
 import { reset } from '@core/styles/reset';
-import { RouterProvider } from 'react-router-dom';
 import { paletteAtom } from '@shared/atoms/paletteAtom';
+import { RouterProvider } from 'react-router-dom';
 
 const App = () => {
   const palette = useAtomValue(paletteAtom);

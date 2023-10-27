0a43929cbcc7c08f529c6eeb9075d22f8a958623
김성욱 <sungwook@MacAir-2.local>
2023-10-28 00:25:50 +0900

    feat: layout background, background-color 다시 적용

--- app/src/@core/layouts/MainLayout/mainLayoutGlobalStyle.tsx
+++ app/src/@core/layouts/MainLayout/mainLayoutGlobalStyle.tsx
@@ -2,10 +2,10 @@ import { Theme, css } from '@emotion/react';
 
 export const mainLayoutGlobalStyle = (theme: Theme) => css`
   html {
-    background: ${theme.colors.mono.white};
+    background-color: ${theme.colors.mono.white};
+    background: ${theme.colors.background.main.default};
     background-repeat: no-repeat;
     background-attachment: fixed;
-    background-color: '#fff';
   }
   body {
     background: ${theme.colors.background.main.default};

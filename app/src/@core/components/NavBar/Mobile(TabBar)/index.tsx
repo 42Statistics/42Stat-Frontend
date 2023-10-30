20967f1ee3c47b1da01b29993209366f1e948bce
Yongjun Park <yopark@student.42seoul.kr>
2023-10-30 13:29:35 +0900

    design: 탭바 useIsSmallDevice 삭제

--- app/src/@core/components/NavBar/Mobile(TabBar)/index.tsx
+++ app/src/@core/components/NavBar/Mobile(TabBar)/index.tsx
@@ -1,5 +1,4 @@
 import styled from '@emotion/styled';
-import { useMediaQuery } from 'react-responsive';
 
 import { TabItem } from '@core/components/NavBar/Mobile(TabBar)/TabItem';
 import { useNavRoutes } from '@core/components/NavBar/hooks/useNavRoutes';
@@ -7,10 +6,9 @@ import { HStack } from '@shared/ui-kit';
 
 export const TabBar = () => {
   const { NAV_ROUTES } = useNavRoutes();
-  const isSmallDevice = useIsSmallDevice();
 
   return (
-    <Layout isSmallDevice={isSmallDevice}>
+    <Layout>
       <HStack as="ul" w="100%" justify="space-around">
         <TabItem route={NAV_ROUTES.HOME} />
         <TabItem route={NAV_ROUTES.PROFILE} />
@@ -23,18 +21,11 @@ export const TabBar = () => {
   );
 };
 
-const useIsSmallDevice = () => useMediaQuery({ maxWidth: '480px' });
-
-type LayoutProps = {
-  isSmallDevice: boolean;
-};
-
-const Layout = styled.nav<LayoutProps>`
+const Layout = styled.nav`
   position: fixed;
   bottom: 0;
   width: 100%;
-  padding: ${({ isSmallDevice }) =>
-    isSmallDevice ? '1rem 0' : '0.4rem 2rem 1rem 2rem'};
+  padding: 0.4rem 1rem 1.2rem 1rem;
   background-color: ${({ theme }) => theme.colors.mono.white};
   user-select: none;
 `;

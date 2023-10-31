0148129bb8dc5d0d2b4df9c31267eb1f94710f13
Yongjun Park <yopark@student.42seoul.kr>
2023-10-31 18:57:24 +0900

    fix(Leaderboard): select key promo & coalition 정보 추가

--- app/src/Leaderboard/hooks/useGetSelectKey.ts
+++ app/src/Leaderboard/hooks/useGetSelectKey.ts
@@ -5,10 +5,23 @@ import { toLeaderboardArgs } from '@/Leaderboard/utils/toLeaderboardArgs';
 export const useGetSelectKey = () => {
   const { pathname } = useLocation();
   const [searchParams] = useSearchParams();
-  const { dateTemplate } = toLeaderboardArgs(searchParams);
+  const { dateTemplate, promo, coalitionId } = toLeaderboardArgs(searchParams);
 
-  const promoSelectKey = 'promo_' + pathname + dateTemplate;
-  const coalitionSelectKey = 'coalition_' + pathname + dateTemplate;
+  const promoSelectKey = [
+    'promo',
+    pathname,
+    dateTemplate,
+    promo,
+    coalitionId?.toString() ?? '',
+  ].join('&');
+
+  const coalitionSelectKey = [
+    'coalition',
+    pathname,
+    dateTemplate,
+    promo,
+    coalitionId?.toString() ?? '',
+  ].join('&');
 
   return { promoSelectKey, coalitionSelectKey };
 };

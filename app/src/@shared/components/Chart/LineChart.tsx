3337db7261d2fb4fc5f4466df188fb6b06abbafd
Yongjun Park <yopark@student.42seoul.kr>
2023-11-02 12:46:30 +0900

    feat(LineChart): enable animation

--- app/src/@shared/components/Chart/LineChart.tsx
+++ app/src/@shared/components/Chart/LineChart.tsx
@@ -15,11 +15,6 @@ export const LineChart = ({
   const theme = useTheme();
 
   const lineChartOptions: ApexCharts.ApexOptions = {
-    chart: {
-      animations: {
-        enabled: false, // curve smooth일 때 간헐적으로 curve straight 애니메이션이 적용되는 문제가 있어서 비활성화
-      },
-    },
     colors: [theme.colors.chart.primary.default],
     stroke: {
       curve: 'smooth',

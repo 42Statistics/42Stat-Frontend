99b291d386a556c83b56244c7ce6bfb12bf30c1f
김성욱 <sungwook@MacAir-2.local>
2023-11-10 21:58:17 +0900

    feat: init GrassActivity

---
+++ app/src/Profile/dashboard-contents/General/GrassActivity/DailyActivity.tsx
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+
+export const DailyActivity = () => {
+  return <Layout></Layout>;
+};
+
+const Layout = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;

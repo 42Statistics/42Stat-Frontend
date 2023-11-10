1822daec32d6927d7550ea9175c124ea7a7b19e8
김성욱 <sungwook@MacAir-2.local>
2023-11-10 22:54:37 +0900

    feat: grass-activity api 연결

--- app/src/Profile/dashboard-contents/General/GrassActivity/index.tsx
+++ app/src/Profile/dashboard-contents/General/GrassActivity/index.tsx
@@ -1,7 +1,6 @@
 import { useQuery } from '@apollo/client';
-import { useContext } from 'react';
+import styled from '@emotion/styled';
 
-import { UserProfileContext } from '@/Profile/contexts/UserProfileContext';
 import { TotalActivity } from '@/Profile/dashboard-contents/General/GrassActivity/TotalActivity';
 import { DailyActivity } from '@/Profile/dashboard-contents/General/GrassActivity/DailyActivity';
 import {
@@ -9,40 +8,77 @@ import {
   DashboardContentLoading,
   DashboardContentNotFound,
 } from '@shared/components/DashboardContentView/Error';
+import { UserProfileContext } from '@/Profile/contexts/UserProfileContext';
+import { gql } from '@shared/__generated__';
+import { useContext, useEffect } from 'react';
 
-// const GET_PERSONAL_ACTIVITY_LOG = gql(/* GraphQL */ `
-//   query GetPersonalActivityLog($login: String!) {
-//   }
-// `);
+const GET_PERSONAL_ACTIVITY_LOG = gql(/* GraphQL */ `
+  query GetPersonalActivityLog(
+    $login: String!
+    $args: [DailyActivityDetailRecordIdWithType!]!
+  ) {
+    getPersonalGeneral(login: $login) {
+      dailyActivityDetailRecords(args: $args) {
+        ... on DailyEventDetailRecord {
+          name
+          location
+          beginAt
+          endAt
+        }
+        ... on DailyEvaluationDetailRecord {
+          type
+          teamId
+          leaderLogin
+          projectName
+          beginAt
+          filledAt
+        }
+      }
+    }
+  }
+`);
 
-export const CoalitionScore = () => {
+export const GrassActivity = () => {
   const { login } = useContext(UserProfileContext);
-
+  // const grassData = useAtomValue(grassAtom);
+  //클릭된 잔디 상태 불러오기
   const title = '활동 내역';
+  const { loading, error, data, refetch } = useQuery(
+    GET_PERSONAL_ACTIVITY_LOG,
+    {
+      variables: {
+        login,
+        args: [{ type: 'CORRECTOR', id: 124 }] /* 수정 필요 */ ?? undefined,
+      },
+    },
+  );
+
+  //불러온 상태에 따라 refetch
+  // useEffect(() => {
+  //   refetch(grassData);
+  // }, [grassData]);
+
+  if (loading) {
+    return <DashboardContentLoading title={title} />;
+  }
+  if (error) {
+    return <DashboardContentBadRequest title={title} message={error.message} />;
+  }
+  if (!data) {
+    return <DashboardContentNotFound title={title} />;
+  }
+
+  return (
+    <Layout>
+      <TotalActivity />
+      <DailyActivity />
+    </Layout>
+  );
 };
-// const { loading, error, data } = useQuery(GET_PERSONAL_ACTIVITY_LOG);
 
-// if (loading) {
-//   return <DashboardContentLoading title={title} />;
-// }
-// if (error) {
-//   return <DashboardContentBadRequest title={title} message={error.message} />;
-// }
-// if (!data) {
-//   return <DashboardContentNotFound title={title} />;
-// }
-
-//   return (
-//     <Layout>
-//       <TotalActivity />
-//       <DailyActivity />
-//     <Layout/>
-//   );
-// };
-
-// const Layout = styled.div`
-// 	display: flex;
-// 	flex-direction: column;
-// 	align-items: center;
-// 	width: 100%;
-// `;
+const Layout = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;

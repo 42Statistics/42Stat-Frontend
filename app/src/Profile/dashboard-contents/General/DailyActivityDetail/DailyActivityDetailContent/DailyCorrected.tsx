76d41c2511cfcf9e8783671e48d8225328327e93
Yongjun Park <yopark@student.42seoul.kr>
2023-12-04 03:39:13 +0900

    refactor(DailyActivityDetail): TimelineItem, Scrollable 이용

--- app/src/Profile/dashboard-contents/General/DailyActivityDetail/DailyActivityDetailContent/DailyCorrected.tsx
+++ app/src/Profile/dashboard-contents/General/DailyActivityDetail/DailyActivityDetailContent/DailyCorrected.tsx
@@ -1,11 +1,13 @@
 import { useTheme } from '@emotion/react';
-import styled from '@emotion/styled';
+import dayjs from 'dayjs';
 import { useContext } from 'react';
+import { Link } from 'react-router-dom';
 
 import { ReactComponent as MdCorrected } from '@/Profile/assets/activity/corrected.svg';
 import { UserProfileContext } from '@/Profile/contexts/UserProfileContext';
-import { BoldText, CaptionText, HStack, Text, VStack } from '@shared/ui-kit';
-import { useNavigate } from 'react-router-dom';
+import { ROUTES } from '@shared/constants/routes';
+import { BoldText, CaptionText, Text, VStack } from '@shared/ui-kit';
+import { TimelineItem } from '../TimelineItem';
 
 type DailyCorrectedProps = {
   data: {
@@ -15,68 +17,27 @@ type DailyCorrectedProps = {
     projectName: string;
     teamId: number;
   };
+  color: string;
 };
 
-export const DailyCorrected = ({ data }: DailyCorrectedProps) => {
+export const DailyCorrected = ({ data, color }: DailyCorrectedProps) => {
   const { beginAt, filledAt, correctorLogin, projectName, teamId } = data;
-  const { coalition, login } = useContext(UserProfileContext);
+  const { login } = useContext(UserProfileContext);
   const theme = useTheme();
-  const navigate = useNavigate();
-
-  const color = coalition?.color ?? theme.colors.accent.default;
-  const start = new Date(beginAt);
-  const end = new Date(filledAt);
-
-  const handleClick = () => {
-    navigate(`/team/${teamId}`);
-  };
-
-  const getTimeFormat = (time: Date) => {
-    const hours = time.getHours().toString().padStart(2, '0');
-    const minutes = time.getMinutes().toString().padStart(2, '0');
-    return `${hours}:${minutes}`;
-  };
 
   return (
-    <>
-      <HStack align="start" spacing="1.5rem">
-        <IconWhiteLayout>
-          <IconLayout bgColor={color}>
-            <MdCorrected width={15} height={15} />
-          </IconLayout>
-        </IconWhiteLayout>
-        <VStack spacing="0.2rem" align="start" style={{ marginTop: '0.5rem' }}>
+    <TimelineItem icon={<MdCorrected width={15} height={15} />} color={color}>
+      <VStack spacing="0.2rem" align="start" style={{ marginTop: '0.5rem' }}>
+        <Link to={ROUTES.TEAM_OF(teamId)}>
           <BoldText
             color={theme.colors.chart.primary.default}
-            onClick={handleClick}
-            style={{ cursor: 'pointer' }}
           >{`[피평가] ${correctorLogin} -> ${login}`}</BoldText>
-          <Text>{projectName}</Text>
-          <CaptionText>{`${getTimeFormat(start)} - ${getTimeFormat(
-            end,
-          )}`}</CaptionText>
-        </VStack>
-      </HStack>
-    </>
+        </Link>
+        <Text>{projectName}</Text>
+        <CaptionText>{`${dayjs(beginAt).format('HH:mm')} ~ ${dayjs(
+          filledAt,
+        ).format('HH:mm')}`}</CaptionText>
+      </VStack>
+    </TimelineItem>
   );
 };
-
-const IconLayout = styled.div<{ bgColor: string }>`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 3rem;
-  height: 3rem;
-  border-radius: 50%;
-  background-color: ${({ bgColor }) => bgColor};
-`;
-
-const IconWhiteLayout = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 3.5rem;
-  height: 3.5rem;
-  border-radius: 50%;
-  background-color: ${({ theme }) => theme.colors.background.box.default};
-`;
--- app/src/Profile/dashboard-contents/General/DailyActivityDetail/DailyActivityDetailContent/DailyEvent.tsx
+++ app/src/Profile/dashboard-contents/General/DailyActivityDetail/DailyActivityDetailContent/DailyEvent.tsx
@@ -1,58 +1,25 @@
-import { useTheme } from '@emotion/react';
-import styled from '@emotion/styled';
-import { useContext } from 'react';
-
 import { ReactComponent as MdEvent } from '@/Profile/assets/activity/event.svg';
-import { UserProfileContext } from '@/Profile/contexts/UserProfileContext';
-import { BoldText, CaptionText, HStack, Text, VStack } from '@shared/ui-kit';
+import { BoldText, CaptionText, Text, VStack } from '@shared/ui-kit';
+import { TimelineItem } from '../TimelineItem';
 
 type DailyEventProps = {
   data: {
     name: string;
     location: string;
   };
+  color: string;
 };
 
-export const DailyEvent = ({ data }: DailyEventProps) => {
+export const DailyEvent = ({ data, color }: DailyEventProps) => {
   const { name, location } = data;
-  const { coalition } = useContext(UserProfileContext);
-  const theme = useTheme();
-  const color = coalition?.color ?? theme.colors.accent.default;
 
   return (
-    <>
-      <HStack align="start" spacing="1.5rem">
-        <IconWhiteLayout>
-          <IconLayout bgColor={color}>
-            <MdEvent width={15} height={15} />
-          </IconLayout>
-        </IconWhiteLayout>
-        <VStack spacing="0.2rem" align="start" style={{ marginTop: '0.5rem' }}>
-          <BoldText>{name}</BoldText>
-          <Text>{location}</Text>
-          <CaptionText>16:30 - 17:00</CaptionText>
-        </VStack>
-      </HStack>
-    </>
+    <TimelineItem icon={<MdEvent width={15} height={15} />} color={color}>
+      <VStack spacing="0.2rem" align="start" style={{ marginTop: '0.5rem' }}>
+        <BoldText>{name}</BoldText>
+        <Text>{location}</Text>
+        <CaptionText>16:30 - 17:00</CaptionText>
+      </VStack>
+    </TimelineItem>
   );
 };
-
-const IconLayout = styled.div<{ bgColor: string }>`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 3rem;
-  height: 3rem;
-  border-radius: 50%;
-  background-color: ${({ bgColor }) => bgColor};
-`;
-
-const IconWhiteLayout = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 3.5rem;
-  height: 3.5rem;
-  border-radius: 50%;
-  background-color: ${({ theme }) => theme.colors.background.box.default};
-`;

cc79f89a04719ffaaa7aa77475e03121eb312ffc
Yongjun Park <yopark@student.42seoul.kr>
2023-12-06 04:35:04 +0900

    feat: change toolbar auto-selected to pan

--- app/src/@shared/components/Chart/hooks/useDefaultOptions.ts
+++ app/src/@shared/components/Chart/hooks/useDefaultOptions.ts
@@ -35,7 +35,7 @@ const defaultOptions: ApexCharts.ApexOptions = {
         pan: true,
         reset: true,
       },
-      autoSelected: 'zoom',
+      autoSelected: 'pan',
     },
     fontFamily:
       "'Pretendard Variable', Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, 'Helvetica Neue', 'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', sans-serif",

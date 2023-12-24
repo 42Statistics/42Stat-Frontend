fe107250cf9619095c85c3a06ac6c22dc8a8ce7d
김성욱 <sungwook@MacAir-2.local>
2023-12-24 15:55:20 +0900

    refactor: dailyActivityTimeline 목적에 맞게 변경

--- app/src/Profile/dashboard-contents/General/DailyActivityDetail/index.tsx
+++ app/src/Profile/dashboard-contents/General/DailyActivityDetail/index.tsx
@@ -31,7 +31,7 @@ export const DailyActivityDetail = () => {
   const title = '일별 활동 내역';
   const description = dayjs(date).format('YYYY년 M월 D일');
 
-  if (date === '' || login !== activityLogin) {
+  if (date === '' || activityLogin !== login) {
     return <DashboardContentLoading title={title} />;
   }
 
@@ -43,28 +43,16 @@ export const DailyActivityDetail = () => {
     );
   }
 
-  if (dailyRecords.length === 0) {
-    return (
-      <DashboardContent
-        title={title}
-        description={description}
-        type="Scrollable"
-      >
-        <VStack w="100%" align="start">
-          <DailyLogTime dailyLogtime={dailyLogtime} color={color} />
-        </VStack>
-      </DashboardContent>
-    );
-  }
-
   // useQuery를 조건부로 사용할 수 없기 때문에, date === '', records.length === 0 등의 조건을 먼저 검사하고,
   // 데이터가 정제된 상태로 하위 컴포넌트인 DailyActivityTimeline 내에서 useQuery를 사용하도록 해야 합니다.
-  // 물론 DailyActivityTimeline이라는 네이밍이 더 이상 유효하지 않아서 수정이 필요합니다.
   return (
-    <DailyActivityTimeline
-      title={title}
-      description={description}
-      records={{ dailyRecords, dailyLogtime }}
-    />
+    <DashboardContent title={title} description={description} type="Scrollable">
+      <VStack w="100%" align="start">
+        <DailyLogTime dailyLogtime={dailyLogtime} color={color} />
+        {dailyRecords.length !== 0 && (
+          <DailyActivityTimeline dailyRecords={dailyRecords} />
+        )}
+      </VStack>
+    </DashboardContent>
   );
 };
